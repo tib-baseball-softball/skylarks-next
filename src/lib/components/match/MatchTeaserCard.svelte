e682254588822b9555a352f6ca69496cedd8b359
David Battefeld <davidbattefeld@gmail.com>
2024-04-19 21:47:14 +0200

    feat: introduce match teaser card row

--- src/lib/components/match/MatchTeaserCard.svelte
+++ src/lib/components/match/MatchTeaserCard.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
     import type {Match} from "../../../../../bsm.js/src/model/Match";
+    import {LogoUtility} from "$lib/service/LogoUtility";
+    import MatchTeaserCardRow from "$lib/components/match/MatchTeaserCardRow.svelte";
+    import {MatchUtility} from "$lib/service/MatchUtility";
+    import {GameWinner} from "$lib/enum/GameWinner";
 
     export let match: Match
+
+    const awayLogo = LogoUtility.getLogoPathForTeamName(match.away_team_name)
+    const homeLogo = LogoUtility.getLogoPathForTeamName(match.home_team_name)
+
+    const winner = MatchUtility.getWinnerForMatch(match)
 </script>
 
 <a class="block card card-hover variant-ghost-surface" href="gamecenter/game-detail/{match.id}">
@@ -11,15 +20,7 @@
     </header>
     <hr class="mt-2 mx-3"/>
     <section class="p-4">
-        <div class="flex justify-between items-center">
-            <img src="/berlin_skylarks_logo.svg" alt="team logo" width="50">
-            <span>{match.away_team_name}</span>
-            <span>{match.away_runs ?? ""}</span>
-        </div>
-        <div class="flex justify-between items-center">
-            <img src="/berlin_skylarks_logo.svg" alt="team logo" width="50">
-            <span>{match.home_team_name}</span>
-            <span>{match.home_runs ?? ""}</span>
-        </div>
+        <MatchTeaserCardRow logo="{awayLogo}" teamName="{match.away_team_name}" score="{match.away_runs}" subduedScore="{winner === GameWinner.home}"/>
+        <MatchTeaserCardRow logo="{homeLogo}" teamName="{match.home_team_name}" score="{match.home_runs}" subduedScore="{winner === GameWinner.away}"/>
     </section>
 </a>
---
+++ src/lib/components/match/MatchTeaserCardRow.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let logo: string
+    export let teamName: string
+    export let score: string | null
+    export let subduedScore: boolean
+</script>
+
+<div class="flex justify-between items-center">
+    <img src="{logo}" alt="team logo for {teamName}" width="50"/>
+    <span>{teamName}</span>
+    <span class:text-surface-600-300-token={subduedScore} class="text-2xl font-extrabold ">{score ?? " "}</span>
+</div>
