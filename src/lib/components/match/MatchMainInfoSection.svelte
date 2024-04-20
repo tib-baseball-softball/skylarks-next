c1b817900d36628c40286b0d2481d943693e5a46
David Battefeld <davidbattefeld@gmail.com>
2024-04-20 21:25:19 +0200

    feat: add match main info on detail page

---
+++ src/lib/components/match/MatchMainInfoSection.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import {LogoUtility} from "$lib/service/LogoUtility";
+    import {MatchUtility} from "$lib/service/MatchUtility";
+    import {DateTimeUtility} from "$lib/service/DateTimeUtility";
+    import type {Match} from "../../../../../bsm.js/src/model/Match";
+
+    export let match: Match
+
+    const awayLogo = LogoUtility.getLogoPathForTeamName(match.away_team_name)
+    const homeLogo = LogoUtility.getLogoPathForTeamName(match.home_team_name)
+
+    const winner = MatchUtility.getWinnerForMatch(match)
+    const matchDate = DateTimeUtility.parseDateFromBSMString(match.time)
+</script>
+
+<div class="card variant-ghost-primary px-2 md:px-5">
+    <header class="pt-2 pb-3">
+        <h2 class="h4 font-medium">Eckdaten</h2>
+    </header>
+    <div class="flex justify-center">
+        <div class="grid grid-cols-3 sm:w-[80%] place-items-center gap-3">
+            <img class="w-14 xl:w-24" src="{awayLogo}" alt="team logo for {match.away_team_name}"/>
+
+            <div class="font-bold text-4xl lg:text-6xl xl:text-7xl">
+                <span>{match.away_runs}</span>
+                <span>&nbsp;-&nbsp;</span>
+                <span>{match.home_runs}</span>
+            </div>
+
+            <img class="w-14 xl:w-24" src="{homeLogo}" alt="team logo for {match.home_team_name}"/>
+
+            <div>{match.away_team_name}</div>
+            <div></div>
+            <div>{match.home_team_name}</div>
+        </div>
+    </div>
+    <div class="grid grid-cols-2 place-items-stretch gap-3 md:gap-8 my-2 md:my-5 pb-2 sm:pb-4 lg:pb-5">
+        <div class="card variant-ghost-primary dark:variant-soft-primary p-3">
+            <p class="mb-2 font-medium">Datum</p>
+            <p>{DateTimeUtility.dateTimeFormatMedium.format(matchDate)}</p>
+        </div>
+        <div class="card variant-ghost-primary dark:variant-soft-primary p-3">
+            <p class="mb-2 font-medium">Status</p>
+            <p>{match.human_state}</p>
+        </div>
+    </div>
+
+</div>
--- src/routes/gamecenter/game-detail/[id]/+page.svelte
+++ src/routes/gamecenter/game-detail/[id]/+page.svelte
@@ -1,21 +1,39 @@
 <script lang="ts">
     import {ProgressRadial, Tab, TabGroup} from "@skeletonlabs/skeleton";
+    import MatchMainInfoSection from "$lib/components/match/MatchMainInfoSection.svelte";
 
     export let data
 
+    $: match = data.match
     let tabSet: number = 0
 </script>
 
-<p>This is the detail for page with ID {data.match.id}</p>
+<h1 class="h2 mt-3">Details zu Spiel {match.match_id}</h1>
+<section class="my-3 lg:my-5">
+    <MatchMainInfoSection {match}/>
+</section>
 
 <section>
-    <TabGroup>
-        <Tab bind:group={tabSet} name="tab1" value={0}>Spieldaten</Tab>
+    <TabGroup justify="justify-center">
+        <Tab bind:group={tabSet} name="tab1" value={0}>weitere Spieldaten</Tab>
         <Tab bind:group={tabSet} name="tab2" value={1}>Boxscore</Tab>
         <!-- Tab Panels --->
         <svelte:fragment slot="panel">
             {#if tabSet === 0}
-                (tab panel 1 contents)
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-stretch gap-3 md:gap-4 lg:gap-5">
+                    <div class="card variant-ghost-surface p-3">
+                        <p class="mb-2 font-medium">Info</p>
+                        <p>(content)</p>
+                    </div>
+                    <div class="card variant-ghost-surface p-3">
+                        <p class="mb-2 font-medium">Info</p>
+                        <p>(content)</p>
+                    </div>
+                    <div class="card variant-ghost-surface p-3">
+                        <p class="mb-2 font-medium">Info</p>
+                        <p>(content)</p>
+                    </div>
+                </div>
             {:else if tabSet === 1}
                 {#await data.singleGameStats}
                     <ProgressRadial />
