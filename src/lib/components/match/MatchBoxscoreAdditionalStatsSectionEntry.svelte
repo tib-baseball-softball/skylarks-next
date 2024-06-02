7b5c7955c24824925f6d46a222b8c949399e8f9d
David Battefeld <davidbattefeld@gmail.com>
2024-06-02 16:15:15 +0200

    feat: add components for additional stats

---
+++ src/lib/components/match/MatchBoxscoreAdditionalStatsSectionEntry.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import type {AdditionalStat} from "bsm.js";
+
+    export let entries: AdditionalStat[]
+</script>
+
+<span class="entries">
+    {#each entries as entry}
+        <span>{entry.person.last_name} {entry.person.first_name.charAt(0)}.
+            {#if entry.sum > 1}
+                ({entry.sum})
+            {/if}
+        </span>
+    {/each}
+</span>
+
+<style>
+    .entries span:not(:last-child)::after {
+        content: ", ";
+    }
+</style>
