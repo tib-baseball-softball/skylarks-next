f42eb22f00e2d78e85f1daaaa1b1aefaead9067a
David Battefeld <davidbattefeld@gmail.com>
2024-06-23 18:12:57 +0200

    feat: add new cards to route

--- src/routes/gamecenter/game-detail/[id]/+page.svelte
+++ src/routes/gamecenter/game-detail/[id]/+page.svelte
@@ -2,6 +2,9 @@
     import {ProgressRadial, Tab, TabGroup} from "@skeletonlabs/skeleton";
     import MatchMainInfoSection from "$lib/components/match/MatchMainInfoSection.svelte";
     import MatchBoxscoreSection from "$lib/components/boxscore/MatchBoxscoreSection.svelte";
+    import MatchDetailStatsCard from "$lib/components/match/MatchDetailStatsCard.svelte";
+    import MatchDetailLocationCard from "$lib/components/match/MatchDetailLocationCard.svelte";
+    import MatchDetailOfficialsCard from "$lib/components/match/MatchDetailOfficialsCard.svelte";
 
     export let data
 
@@ -14,7 +17,7 @@
     <MatchMainInfoSection {match}/>
 </section>
 
-<section>
+<section class="mb-5">
     <TabGroup justify="justify-center">
         <Tab bind:group={tabSet} name="tab1" value={0}>weitere Spieldaten</Tab>
         <Tab bind:group={tabSet} name="tab2" value={1}>Boxscore</Tab>
@@ -22,18 +25,11 @@
         <svelte:fragment slot="panel">
             {#if tabSet === 0}
                 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-stretch gap-3 md:gap-4 lg:gap-5">
-                    <div class="card variant-ghost-surface p-3">
-                        <p class="mb-2 font-medium">Info</p>
-                        <p>(content)</p>
-                    </div>
-                    <div class="card variant-ghost-surface p-3">
-                        <p class="mb-2 font-medium">Info</p>
-                        <p>(content)</p>
-                    </div>
-                    <div class="card variant-ghost-surface p-3">
-                        <p class="mb-2 font-medium">Info</p>
-                        <p>(content)</p>
-                    </div>
+                    <MatchDetailStatsCard {match}/>
+
+                    <MatchDetailLocationCard {match}/>
+
+                    <MatchDetailOfficialsCard {match}/>
                 </div>
             {:else if tabSet === 1}
                 {#await data.singleGameStats}
