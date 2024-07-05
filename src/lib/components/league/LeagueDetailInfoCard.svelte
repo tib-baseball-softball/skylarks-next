a7d36d165914b1816501f2d5cb3cc5648dcbb9c9
David Battefeld <davidbattefeld@gmail.com>
2024-07-05 12:22:10 +0200

    feat: add infobox to league detail

---
+++ src/lib/components/league/LeagueDetailInfoCard.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import type {LeagueGroup} from "bsm.js";
+    import {
+        FileLinesOutline, FlagOutline, TagOutline, UsersGroupOutline
+    } from "flowbite-svelte-icons";
+
+    export let leagueGroup: LeagueGroup
+</script>
+
+<div class="card variant-ghost-surface p-3">
+
+    <div class="flex items-center gap-3 self-end">
+        <FileLinesOutline/>
+        <div>
+            <p>{leagueGroup.name}</p>
+            <p class="text-sm font-light">Name</p>
+        </div>
+    </div>
+
+    <hr class="my-2">
+
+    <div class="flex items-center gap-3">
+        <TagOutline/>
+        <div>
+            <p>{leagueGroup.acronym}</p>
+            <p class="text-sm font-light">Akronym</p>
+        </div>
+    </div>
+
+    <hr class="my-2">
+
+    <div class="flex items-center gap-3">
+        <FlagOutline/>
+        <div>
+            <p>{leagueGroup.league.sport}</p>
+            <p class="text-sm font-light">Sportart</p>
+        </div>
+    </div>
+
+    <hr class="my-2">
+
+    <div class="flex items-center gap-3">
+        <UsersGroupOutline/>
+        <div>
+            <p>{leagueGroup.league.age_group}</p>
+            <p class="text-sm font-light">Altersgruppe</p>
+        </div>
+    </div>
+
+</div>
--- src/routes/ligen/[id]/+page.svelte
+++ src/routes/ligen/[id]/+page.svelte
@@ -2,6 +2,7 @@
     import type {PageServerData} from "../../../../.svelte-kit/types/src/routes/ligen/[id]/$types";
     import {ProgressRadial} from "@skeletonlabs/skeleton";
     import StandingsTable from "$lib/components/table/StandingsTable.svelte";
+    import LeagueDetailInfoCard from "$lib/components/league/LeagueDetailInfoCard.svelte";
 
     export let data: PageServerData
     $: expectedTable = data.table
@@ -10,7 +11,18 @@
 
 <h1 class="h1 my-4">{leagueGroup.name} ({leagueGroup.season})</h1>
 
+<section class="my-5">
+    <h2 class="h3">Informationen</h2>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+
+        <LeagueDetailInfoCard {leagueGroup}/>
+
+    </div>
+</section>
+
 <section class="my-3">
+
+    <h2 class="h3">Tabelle</h2>
     {#await expectedTable}
         <ProgressRadial/>
     {:then table}
@@ -22,3 +34,9 @@
     {/await}
 
 </section>
+
+<style lang="postcss">
+    h2 {
+        @apply mb-2
+    }
+</style>
