ec2ce4fc73d928517598abfc6d9b76b2dc5c1ba6
David Battefeld <davidbattefeld@gmail.com>
2024-08-02 13:43:02 +0200

    feat: add table to team page

--- src/routes/teams/[id]/+page.svelte
+++ src/routes/teams/[id]/+page.svelte
@@ -4,6 +4,7 @@
     import BaseballStatsDatatable from "$lib/components/datatable/BaseballStatsDatatable.svelte";
     import type {StatsDataset} from "$lib/types/StatsDataset";
     import TeamDetailInfoCard from "$lib/components/team/TeamDetailInfoCard.svelte";
+    import StandingsTable from "$lib/components/table/StandingsTable.svelte";
 
     export let data
     $: clubTeam = data.clubTeam as ClubTeam
@@ -21,9 +22,21 @@
 
 <section class="my-5">
     <h2 class="h2">Informationen</h2>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-6 xl:gap-8">
 
         <TeamDetailInfoCard {clubTeam}/>
+
+        {#await data?.table}
+            <ProgressRadial/>
+        {:then table}
+            {#if table}
+                <div class="col-span-2">
+                    <StandingsTable {table}/>
+                </div>
+            {/if}
+        {:catch error}
+            <p>error loading: {error.message}</p>
+        {/await}
 
     </div>
 </section>
