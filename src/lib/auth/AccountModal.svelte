3b3e8236f39e0b3ef815b2f38958af20a4c23c55
David Battefeld <davidbattefeld@gmail.com>
2024-09-13 17:48:40 +0200

    feat(FE): add link to profile to AccountModal

--- src/lib/auth/AccountModal.svelte
+++ src/lib/auth/AccountModal.svelte
@@ -1,43 +1,56 @@
 <script lang="ts">
-    import {authModel, client} from "../pocketbase";
+    import { authModel, client } from "../pocketbase";
 
-    const {parent} = $props()
+    const { parent } = $props();
 
     async function logout() {
         //@ts-ignore
-        parent.onClose()
+        parent.onClose();
         client.authStore.clear();
+    }
+
+    async function closeModal() {
+        // @ts-ignore
+        parent.onClose();
     }
 </script>
 
 <div class="w-modal-slim">
     <div class="card p-4">
-
         <div class="flex mt-1 mb-5 justify-between items-center">
             <p>Eingeloggt als:</p>
-            <div class="badge variant-filled-primary">
+
+            <a
+                href="/account"
+                class="badge variant-filled-primary"
+                onclick={closeModal}
+            >
                 {#if $authModel?.avatar}
                     <img
                         src={client.getFileUrl($authModel, $authModel?.avatar)}
                         alt="profile pic"
                     />
                 {/if}
-                <samp>{`${$authModel?.first_name ?? ""} ${$authModel?.last_name ?? ""}`}</samp>
-            </div>
-
+                <samp
+                    >{`${$authModel?.first_name ?? ""} ${$authModel?.last_name ?? ""}`}</samp
+                >
+            </a>
         </div>
 
         <div class="flex mt-1 mb-5 justify-between items-start">
             <p>Club:</p>
+
             <div class="flex flex-wrap gap-2 justify-end">
                 {#each $authModel?.expand?.club as club}
-                    <span class="badge variant-filled-tertiary">{club.name}</span>
+                    <span class="badge variant-filled-tertiary"
+                        >{club.name}</span
+                    >
                 {/each}
             </div>
-
         </div>
 
-        <button class="btn variant-ghost-primary mb-1" onclick={logout}>Abmelden</button>
-
+        <button class="btn variant-ghost-primary mb-1" onclick={logout}
+            >Abmelden</button
+        >
     </div>
 </div>
