8cd4aba3137e9e5ca2f76af79981cbcf1029026b
David Battefeld <davidbattefeld@gmail.com>
2024-10-06 14:58:47 +0200

    refactor(FE): move event loading to service class for reuse

---
+++ src/lib/service/EventService.ts
@@ -0,0 +1,54 @@
+import type { ExpandedEvent } from "$lib/model/ExpandedResponse";
+import type { PageStore } from "$lib/pocketbase/PageStore";
+import { watchWithPagination } from "$lib/pocketbase/RecordOperations";
+
+export class EventService {
+  /**
+  * Takes a team ID and an URL, construct PocketBase filter string from query parameters
+  * and loads events with realtime.
+  */
+  public async loadEventStore(teamID: string, url: URL, fetch: {
+    (input: RequestInfo | URL, init?: RequestInit): Promise<Response>;
+    (input: string | URL | globalThis.Request, init?: RequestInit): Promise<Response>;
+  }): Promise<PageStore<ExpandedEvent>> {
+    let filter = `team = "${teamID}"`
+
+    let timeframe = url.searchParams.get("timeframe")
+
+    if (!timeframe || timeframe === "next") {
+      filter = filter.concat(`&& starttime >= @todayStart`)
+    } else if (timeframe === "past") {
+      filter = filter.concat(`&& starttime <= @todayStart`)
+    }
+
+    const showTypes = url.searchParams.get("type") ?? "any"
+
+    if (showTypes !== "any") {
+      filter = filter.concat(`&& type = "${showTypes}"`)
+    }
+
+    // add sort parameter
+
+    let sort = "+starttime"
+
+    if (url.searchParams.get("sort") === "desc") {
+      sort = "-starttime"
+    }
+
+    // check pagination info
+
+    const pageNumber = Number(url.searchParams.get("page")) ?? 1;
+
+    return await watchWithPagination<ExpandedEvent>(
+      "events",
+      {
+        filter: filter,
+        sort: sort,
+        expand: "participations_via_event.user, attire",
+        fetch: fetch
+      },
+      pageNumber,
+      6,
+    );
+  }
+}
--- src/routes/(protected)/account/team/[id]/+page.ts
+++ src/routes/(protected)/account/team/[id]/+page.ts
@@ -1,9 +1,10 @@
 import { client } from "$lib/pocketbase";
 import { error } from "@sveltejs/kit";
-import { watchWithPagination } from "$lib/pocketbase/RecordOperations";
-import type { ExpandedEvent, ExpandedTeam } from "$lib/model/ExpandedResponse.js";
+import type { ExpandedTeam } from "$lib/model/ExpandedResponse.js";
+import type { PageLoad } from "./$types";
+import { EventService } from "$lib/service/EventService";
 
-export const load = async ({ fetch, parent, params, url, depends }) => {
+export const load = (async ({ fetch, parent, params, url, depends }) => {
   const data = await parent();
   const teams: ExpandedTeam[] = await data.teams;
 
@@ -16,47 +17,8 @@ export const load = async ({ fetch, parent, params, url, depends }) => {
   }
   if (!team) throw error(404, "Team not found");
 
-  //Construct PocketBase filter string from query parameters
-
-  let filter = `team = "${team.id}"`
-
-  let timeframe = url.searchParams.get("timeframe")
-
-  if (!timeframe || timeframe === "next") {
-    filter = filter.concat(`&& starttime >= @todayStart`)
-  } else if (timeframe === "past") {
-    filter = filter.concat(`&& starttime <= @todayStart`)
-  }
-
-  const showTypes = url.searchParams.get("type") ?? "any"
-
-  if (showTypes !== "any") {
-    filter = filter.concat(`&& type = "${showTypes}"`)
-  }
-
-  // add sort parameter
-
-  let sort = "+starttime"
-
-  if (url.searchParams.get("sort") === "desc") {
-    sort = "-starttime"
-  }
-
-  // check pagination info
-
-  const pageNumber = Number(url.searchParams.get("page")) ?? 1;
-
-  const events = await watchWithPagination<ExpandedEvent>(
-    "events",
-    {
-      filter: filter,
-      sort: sort,
-      expand: "participations_via_event.user, attire",
-      fetch: fetch
-    },
-    pageNumber,
-    6,
-  );
+  const eventService = new EventService()
+  const events = await eventService.loadEventStore(team.id, url, fetch)
 
   depends("event:list")
 
@@ -64,4 +26,4 @@ export const load = async ({ fetch, parent, params, url, depends }) => {
     team: team,
     events: events,
   };
-};
+}) satisfies PageLoad
