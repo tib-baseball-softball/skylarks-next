4faae9e8cb82d45d3fc099f702e791613018837b
David Battefeld <davidbattefeld@gmail.com>
2024-11-01 20:06:19 +0100

    feat(FE): improve styling for LoginForm

--- src/lib/auth/LoginForm.svelte
+++ src/lib/auth/LoginForm.svelte
@@ -16,7 +16,6 @@
 
   const form = $state({
     email: "",
-    name: "",
     password: "",
     passwordConfirm: "",
   })
@@ -48,26 +47,39 @@
 </script>
 
 {#snippet signin()}
-    <input class="input" bind:value={form.email} required type="text" placeholder="email"/>
-    <input
-            bind:value={form.password}
-            class="input"
-            required
-            type="password"
-            placeholder="password"
-            minlength="8"
-            maxlength="72"
-    />
+    <label class="label">
+        <span class="ps-2">Your email</span>
+        <input class="input" bind:value={form.email} required type="text" placeholder="name@provider.com"/>
+    </label>
 
-    <button class="btn variant-ghost-primary" type="submit" onclick={() => (signup = false)}>Sign In</button>
+    <label class="label">
+        <span class="ps-2">Your password</span>
+        <input
+                bind:value={form.password}
+                class="input"
+                required
+                type="password"
+                placeholder="**********"
+                minlength="8"
+                maxlength="72"
+        />
+    </label>
+
+    <button class="btn variant-ghost-primary" type="submit" onclick={() => (signup = false)}>
+        Login to your account
+    </button>
 {/snippet}
 
 <div class="w-modal">
-    <div class="card p-4">
+    <article class="card p-6">
+        <header class="mb-4">
+            <h2 class="h4 font-semibold">Sign in to DiamondPlanner</h2>
+        </header>
+
         <form onsubmit={submit}>
             {#if passwordLogin}
                 {#if signupAllowed}
-                    <TabGroup>
+                    <TabGroup flex="grow" class="my-2" active="variant-ghost border-b-2 border-surface-900-50-token">
                         <Tab bind:group={tabSet} name="tab1" value={0}>Log In</Tab>
                         <Tab bind:group={tabSet} name="tab2" value={1}>Sign Up</Tab>
 
@@ -78,43 +90,48 @@
 
                             {:else if tabSet === 1}
 
-                                <input
-                                        class="input"
-                                        bind:value={form.email}
-                                        required
-                                        type="email"
-                                        placeholder="email"
-                                />
-                                <input
-                                        class="input"
-                                        bind:value={form.password}
-                                        required
-                                        type="password"
-                                        placeholder="password"
-                                        minlength="8"
-                                        maxlength="72"
-                                />
-                                <input
-                                        class="input"
-                                        bind:value={form.passwordConfirm}
-                                        required
-                                        type="password"
-                                        placeholder="confirm password"
-                                        minlength="8"
-                                        maxlength="72"
-                                />
-                                <input
-                                        class="input"
-                                        bind:value={form.name}
-                                        required
-                                        type="text"
-                                        placeholder="name / label"
-                                />
-                                <input type="hidden" name="register" value={true}/>
-                                <button class="btn variant-ghost-primary" type="submit" onclick={() => (signup = true)}>
-                                    Register
-                                </button>
+                                <label class="label">
+                                    <span class="ps-2">Your email</span>
+                                    <input
+                                            class="input"
+                                            bind:value={form.email}
+                                            required
+                                            type="email"
+                                            placeholder="name@provider.com"
+                                    />
+                                </label>
 
+                                <label class="label">
+                                    <span class="ps-2">Your password</span>
+                                    <input
+                                            class="input"
+                                            bind:value={form.password}
+                                            required
+                                            type="password"
+                                            placeholder="**********"
+                                            minlength="8"
+                                            maxlength="72"
+                                    />
+                                </label>
+
+                                <label class="label">
+                                    <span class="ps-2">Confirm password</span>
+                                    <input
+                                            class="input"
+                                            bind:value={form.passwordConfirm}
+                                            required
+                                            type="password"
+                                            placeholder="confirm password"
+                                            minlength="8"
+                                            maxlength="72"
+                                    />
+                                </label>
+
+                                <input type="hidden" name="register" value={true}/>
+
+                                <button class="btn variant-ghost-primary" type="submit" onclick={() => (signup = true)}>
+                                    Register new account
+                                </button>
                             {/if}
                         </svelte:fragment>
 
@@ -126,27 +143,35 @@
             {/if}
 
             {#await coll.listAuthMethods({$autoCancel: false}) then methods}
+                {#if methods.authProviders.length > 0}
 
-                {#each methods.authProviders as provider}
-                    <button
-                            class="btn variant-ghost-tertiary"
-                            type="button"
-                            onclick={() => submitOAuthRequest(provider)}
-                    >
-                        Sign-in with {provider.name}
-                    </button>
-                {/each}
+                    <p class="px-2 py-2 text-surface-600-300-token">or sign in with</p>
 
+                    {#each methods.authProviders as provider}
+                        <button
+                                class="btn variant-ghost-tertiary"
+                                type="button"
+                                onclick={() => submitOAuthRequest(provider)}
+                        >
+
+                            <span class="capitalize">{provider.name}</span>
+                        </button>
+                    {/each}
+                {/if}
             {:catch error}
                 <!-- pocketbase not working -->
                 <p>There seems to be an error while contacting the backend. Please try again later.</p>
             {/await}
         </form>
-    </div>
+    </article>
 </div>
 
 <style lang="postcss">
     input, .btn {
-        @apply m-2
+        @apply m-2;
+    }
+
+    .label {
+        @apply my-2;
     }
 </style>
