002a3a4d5e1d58de989cf5a4fb5e1ae5b31a8e6b
David Battefeld <davidbattefeld@gmail.com>
2024-11-01 17:03:16 +0100

    fix(FE): fix TS error with server loads on root page

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,17 +1,18 @@
 import type {LayoutServerLoad} from "../../.svelte-kit/types/src/routes/$types";
-import {LeagueGroupAPIRequest} from "bsm.js";
+import {ClubTeamsAPIRequest, LeagueGroupAPIRequest} from "bsm.js";
 import {BSM_API_KEY} from "$env/static/private";
 import {get} from "svelte/store";
 import {preferences} from "$lib/stores";
-import {ClubTeamsAPIRequest} from "bsm.js";
 import {PUBLIC_CLUB_ID} from "$env/static/public";
+import type {AppPreferences} from "$lib/types/AppPreferences";
 
 export const load = (async () => {
     const leagueGroupRequest = new LeagueGroupAPIRequest(BSM_API_KEY)
     const clubTeamRequest = new ClubTeamsAPIRequest(BSM_API_KEY)
+    const appPreferences = get(preferences) as AppPreferences
 
     return {
         clubTeams: clubTeamRequest.getTeamsForClub(Number(PUBLIC_CLUB_ID)),
-        leagueGroups: leagueGroupRequest.getLeagueGroupsForClub(get(preferences).selectedSeason)
+        leagueGroups: leagueGroupRequest.getLeagueGroupsForClub(appPreferences.selectedSeason)
     }
 }) satisfies LayoutServerLoad
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,17 +1,18 @@
-import {MatchAPIRequest} from "bsm.js";
+import {Gameday, MatchAPIRequest} from "bsm.js";
 import {BSM_API_KEY} from "$env/static/private";
 import {PUBLIC_CLUB_ID} from "$env/static/public";
 import {preferences} from "$lib/stores";
 import {get} from "svelte/store";
-import {Gameday} from "bsm.js";
 import type {PageServerLoad} from "../../.svelte-kit/types/src/routes/$types";
+import type {AppPreferences} from "$lib/types/AppPreferences";
 
-export const load = (async ({ fetch }) => {
+export const load = (async ({fetch}) => {
     const matchRequest = new MatchAPIRequest(BSM_API_KEY)
+    const appPreferences = get(preferences) as AppPreferences
 
-    const matchesCurrent = matchRequest.loadGamesForClub(Number(PUBLIC_CLUB_ID), get(preferences).selectedSeason, Gameday.current)
-    const matchesPrevious = matchRequest.loadGamesForClub(Number(PUBLIC_CLUB_ID), get(preferences).selectedSeason, Gameday.previous)
-    const matchesNext = matchRequest.loadGamesForClub(Number(PUBLIC_CLUB_ID), get(preferences).selectedSeason, Gameday.next)
+    const matchesCurrent = matchRequest.loadGamesForClub(Number(PUBLIC_CLUB_ID), appPreferences.selectedSeason, Gameday.current)
+    const matchesPrevious = matchRequest.loadGamesForClub(Number(PUBLIC_CLUB_ID), appPreferences.selectedSeason, Gameday.previous)
+    const matchesNext = matchRequest.loadGamesForClub(Number(PUBLIC_CLUB_ID), appPreferences.selectedSeason, Gameday.next)
 
     return {
         matchesCurrent: matchesCurrent,
