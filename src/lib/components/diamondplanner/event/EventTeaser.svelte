b62d290068e302cdbb4d544bff04d01e07667ae8
David Battefeld <davidbattefeld@gmail.com>
2024-11-15 14:53:01 +0100

    polish: improve contrast for EventTeaser

--- src/lib/components/diamondplanner/event/EventTeaser.svelte
+++ src/lib/components/diamondplanner/event/EventTeaser.svelte
@@ -1,44 +1,44 @@
 <script lang="ts">
-    import EventTypeBadge from "$lib/components/diamondplanner/event/EventTypeBadge.svelte";
-    import type { ExpandedEvent } from "$lib/model/ExpandedResponse";
-    import EventCoreInfo from "./EventCoreInfo.svelte";
-    import EventParticipationSection from "./EventParticipationSection.svelte";
-    import { CloseOutline } from "flowbite-svelte-icons";
+  import EventTypeBadge from "$lib/components/diamondplanner/event/EventTypeBadge.svelte";
+  import type {ExpandedEvent} from "$lib/model/ExpandedResponse";
+  import EventCoreInfo from "./EventCoreInfo.svelte";
+  import EventParticipationSection from "./EventParticipationSection.svelte";
+  import {CloseOutline} from "flowbite-svelte-icons";
 
-    interface props {
-        event: ExpandedEvent;
-        link: boolean;
-    }
+  interface props {
+    event: ExpandedEvent;
+    link: boolean;
+  }
 
-    const { event, link }: props = $props();
+  const {event, link}: props = $props();
 </script>
 
 <article
-    class="card variant-soft-surface shadow-xl text-sm h-full"
-    class:card-hover={link}
+        class="card variant-soft-surface shadow-xl text-sm h-full"
+        class:card-hover={link}
 >
     <a href="/account/event/{event.id}" class:line-through={event.cancelled}>
         <header class="card-header">
             <h2>
-                <EventTypeBadge type={event.type} />
-                <span class="ms-1 font-medium">{event?.title}</span>
+                <EventTypeBadge type={event.type}/>
+                <span class="ms-1 font-medium h4">{event?.title}</span>
             </h2>
         </header>
 
-        <EventCoreInfo {event} classes={"p-4"} />
+        <EventCoreInfo {event} classes={"p-4"}/>
     </a>
 
     <footer class="card-footer">
-        <hr class="my-2" />
+        <hr class="my-2"/>
         {#if event.cancelled}
             <div class="flex justify-end">
                 <span class="badge variant-filled-error">
-                    <CloseOutline />
+                    <CloseOutline/>
                     Cancelled
                 </span>
             </div>
         {:else}
-            <EventParticipationSection {event} />
+            <EventParticipationSection {event}/>
         {/if}
     </footer>
 </article>
--- src/routes/(protected)/account/team/[id]/+page.svelte
+++ src/routes/(protected)/account/team/[id]/+page.svelte
@@ -89,7 +89,7 @@
     </label>
 </div>
 
-<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3">
+<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 lg:gap-4 2xl:gap-5">
     {#each $events.items as event}
         <EventTeaser {event} link={true}/>
     {:else}
