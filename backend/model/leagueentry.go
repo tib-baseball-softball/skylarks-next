7a62759e855a378407a83761b0ce311cf2c8dca2
David Battefeld <davidbattefeld@gmail.com>
2024-11-29 15:55:12 +0100

    style: format go models

--- backend/model/leagueentry.go
+++ backend/model/leagueentry.go
@@ -1,14 +1,14 @@
 package model
 
 type LeagueEntry struct {
-	ID                          int      `json:"id"`
-	CreatedAt                   string   `json:"created_at"`
-	UpdatedAt                   string   `json:"updated_at"`
-	GameDay                     *string  `json:"game_day"`
-	NotCompeting                bool     `json:"not_competing"`
-	State                       string   `json:"state"`
-	HumanGameDay                *string  `json:"human_game_day,omitempty"`
-	HumanLicenseCriteriaState   *string  `json:"human_license_criteria_state,omitempty"`
+	ID                        int     `json:"id"`
+	CreatedAt                 string  `json:"created_at"`
+	UpdatedAt                 string  `json:"updated_at"`
+	GameDay                   *string `json:"game_day"`
+	NotCompeting              bool    `json:"not_competing"`
+	State                     string  `json:"state"`
+	HumanGameDay              *string `json:"human_game_day,omitempty"`
+	HumanLicenseCriteriaState *string `json:"human_license_criteria_state,omitempty"`
+	Team                      *Team   `json:"team,omitempty"`
 	//Field                       *Field   `json:"field,omitempty"`
-	Team                        *Team    `json:"team,omitempty"`
 }
