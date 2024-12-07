015b5d6e7bed2b79e17ad58a2ee85aa72b7fd440
David Battefeld <davidbattefeld@gmail.com>
2024-12-07 19:26:20 +0100

    fix: add dark mode styling for Flatpickr

--- src/lib/components/utility/Flatpickr.svelte
+++ src/lib/components/utility/Flatpickr.svelte
@@ -1,34 +1,34 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import flatpickr from "flatpickr";
-    import "flatpickr/dist/flatpickr.css";
-    import type { Options } from "flatpickr/dist/types/options";
+  import {onMount} from "svelte";
+  import flatpickr from "flatpickr";
+  import "flatpickr/dist/flatpickr.css";
+  import type {Options} from "flatpickr/dist/types/options";
 
-    interface Props {
-        value: string; // flatpickr accepts timestamps, date objects and ISO strings, but we work with strings exclusively
-        options: Options;
-    }
+  interface Props {
+    value: string; // flatpickr accepts timestamps, date objects and ISO strings, but we work with strings exclusively
+    options: Options;
+  }
 
-    let { value = $bindable(), options = {} }: Props = $props();
+  let {value = $bindable(), options = {}}: Props = $props();
 
-    let datepicker: HTMLInputElement;
+  let datepicker: HTMLInputElement;
 
-    onMount(() => {
-        const fp = flatpickr(datepicker, {
-            ...options,
-            defaultDate: value ?? "",
-            onChange: (selectedDates) => {
-                value = selectedDates[0].toISOString();
-            },
-        });
-
-        return () => {
-            fp.destroy();
-        };
+  onMount(() => {
+    const fp = flatpickr(datepicker, {
+      ...options,
+      defaultDate: value ?? "",
+      onChange: (selectedDates) => {
+        value = selectedDates[0].toISOString();
+      },
     });
+
+    return () => {
+      fp.destroy();
+    };
+  });
 </script>
 
-<input class="dark:bg-surface-700" type="text" bind:this={datepicker} {value} />
+<input class="dark:bg-surface-700" type="text" bind:this={datepicker} {value}/>
 
 <style lang="postcss">
     input {
