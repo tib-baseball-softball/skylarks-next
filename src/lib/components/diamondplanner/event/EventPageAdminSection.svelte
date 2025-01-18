62f1bfb488e900dbcf6b3723c762f49b11a207e0
David Battefeld <davidbattefeld@gmail.com>
2025-01-18 18:49:16 +0100

    feat: delete individual events
    closes #13

--- src/lib/components/diamondplanner/event/EventPageAdminSection.svelte
+++ src/lib/components/diamondplanner/event/EventPageAdminSection.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
-  import {
-    CalendarEditOutline,
-    CalendarPlusOutline,
-    EditOutline,
-    InfoCircleOutline,
-    TrashBinOutline
-  } from "flowbite-svelte-icons";
+  import {CalendarEditOutline, CalendarPlusOutline, EditOutline, InfoCircleOutline} from "flowbite-svelte-icons";
   import type {ExpandedEvent} from "$lib/model/ExpandedResponse";
   import {type DrawerSettings, getDrawerStore} from "@skeletonlabs/skeleton";
   import {client} from "$lib/pocketbase/index.svelte";
   import type {EventsUpdate} from "$lib/model/pb-types";
+  import {goto} from "$app/navigation";
+  import DeleteButton from "$lib/components/utility/DeleteButton.svelte";
 
   interface Props {
     event: ExpandedEvent
@@ -42,6 +38,11 @@
     })
     guestPlayerForm.name = ""
   }
+
+  async function deleteEvent(id: string) {
+    await client.collection("events").delete(id)
+    await goto(`/account/team/${event.team}`)
+  }
 </script>
 
 <hr class="mt-6"/>
@@ -51,83 +52,86 @@
 <div
         class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-3"
 >
-    <div class="card variant-ringed-surface">
-        <header class="card-header">
-            <h3 class="h4 font-semibold">Event Details</h3>
-        </header>
+  <div class="card variant-ringed-surface">
+    <header class="card-header">
+      <h3 class="h4 font-semibold">Event Details</h3>
+    </header>
 
-        <section class="p-4 space-y-2">
-            <div class="flex items-center gap-3">
-                <InfoCircleOutline/>
-                <div>
-                    <p class="text-sm font-light">BSM event ID</p>
-                    <p>{event.bsm_id}</p>
-                </div>
-            </div>
+    <section class="p-4 space-y-2">
+      <div class="flex items-center gap-3">
+        <InfoCircleOutline/>
+        <div>
+          <p class="text-sm font-light">BSM event ID</p>
+          <p>{event.bsm_id}</p>
+        </div>
+      </div>
 
-            <div class="flex items-center gap-3">
-                <CalendarPlusOutline/>
-                <div>
-                    <p class="text-sm font-light">Created</p>
-                    <p>{new Date(event.created).toLocaleString()}</p>
-                </div>
-            </div>
+      <div class="flex items-center gap-3">
+        <CalendarPlusOutline/>
+        <div>
+          <p class="text-sm font-light">Created</p>
+          <p>{new Date(event.created).toLocaleString()}</p>
+        </div>
+      </div>
 
-            <div class="flex items-center gap-3">
-                <CalendarEditOutline/>
-                <div>
-                    <p class="text-sm font-light">Last Updated</p>
-                    <p>{new Date(event.updated).toLocaleString()}</p>
-                </div>
-            </div>
-        </section>
-    </div>
+      <div class="flex items-center gap-3">
+        <CalendarEditOutline/>
+        <div>
+          <p class="text-sm font-light">Last Updated</p>
+          <p>{new Date(event.updated).toLocaleString()}</p>
+        </div>
+      </div>
+    </section>
+  </div>
 
-    <div class="card variant-ringed-surface">
-        <header class="card-header">
-            <h3 class="h4 font-semibold">Add Guest player</h3>
-        </header>
+  <div class="card variant-ringed-surface">
+    <header class="card-header">
+      <h3 class="h4 font-semibold">Add Guest player</h3>
+    </header>
 
-        <section class="p-4 space-y-2">
-            <form
-                    onsubmit={submitNewGuestPlayer}
-            >
-                <label class="label">
-                    Name
-                    <input
-                            type="text"
-                            class="input mt-1"
-                            placeholder="Enter the guest player's name"
-                            bind:value={guestPlayerForm.name}
-                    />
-                </label>
-                <button type="submit" class="mt-3 btn variant-soft">Submit
-                </button
-                >
-            </form>
-        </section>
-    </div>
+    <section class="p-4 space-y-2">
+      <form
+              onsubmit={submitNewGuestPlayer}
+      >
+        <label class="label">
+          Name
+          <input
+                  type="text"
+                  class="input mt-1"
+                  placeholder="Enter the guest player's name"
+                  bind:value={guestPlayerForm.name}
+          />
+        </label>
+        <button type="submit" class="mt-3 btn variant-soft">Submit
+        </button
+        >
+      </form>
+    </section>
+  </div>
 
-    <div class="card variant-ringed-surface">
-        <header class="card-header">
-            <h3 class="h4 font-semibold">Actions</h3>
-        </header>
+  <div class="card variant-ringed-surface">
+    <header class="card-header">
+      <h3 class="h4 font-semibold">Actions</h3>
+    </header>
 
-        <section class="p-4 space-y-3">
-            <div class="flex flex-col gap-2 lg:gap-3">
-                <button
-                        class="btn variant-ghost-surface"
-                        onclick={() => drawerStore.open(settings)}
-                >
-                    <EditOutline/>
-                    <span>Edit Event</span>
-                </button>
+    <section class="p-4 space-y-3">
+      <div class="flex flex-col gap-2 lg:gap-3">
+        <button
+                class="btn variant-ghost-surface"
+                onclick={() => drawerStore.open(settings)}
+        >
+          <EditOutline/>
+          <span>Edit Event</span>
+        </button>
 
-                <button class="btn variant-ghost-error">
-                    <TrashBinOutline/>
-                    <span>Delete Event</span>
-                </button>
-            </div>
-        </section>
-    </div>
+        <DeleteButton
+                id={event.id}
+                modelName="Event"
+                action={deleteEvent}
+                classes="variant-ghost-error"
+                buttonText="Delete Event"
+        />
+      </div>
+    </section>
+  </div>
 </div>
