a1bda9b71da461daa3c1cf4c39fcf851d602e781
David Battefeld <davidbattefeld@gmail.com>
2025-01-25 20:15:01 +0100

    fix: filter users by club in ClubForm.svelte

--- src/lib/components/forms/ClubForm.svelte
+++ src/lib/components/forms/ClubForm.svelte
@@ -35,8 +35,9 @@
 
   let selectedAdmins: UsersResponse[] = $derived(form.expand.admins)
 
-  // API rule disallows users external to club
-  const allUsersForClub = client.collection("users").getFullList<UsersResponse>()
+  const allUsersForClub = client.collection("users").getFullList<UsersResponse>({
+    filter: `club ?~ '${$drawerStore.meta.club.id}'`
+  })
 
   async function submitForm(e: SubmitEvent) {
     e.preventDefault();
@@ -113,153 +114,153 @@
 </script>
 
 <article class="p-6">
-    <div class="flex items-center gap-5">
-        <button
-                aria-label="cancel and close"
-                class="btn variant-ghost-surface"
-                onclick={drawerStore.close}
-        >
-            <CloseOutline/>
-        </button>
-        <header class="text-xl font-semibold">
-            {#if form.id}
-                <h2 class="h3">Edit Club "{form?.name}"</h2>
-            {:else}
-                <h2 class="h3">Create new Club</h2>
-            {/if}
-        </header>
-    </div>
+  <div class="flex items-center gap-5">
+    <button
+            aria-label="cancel and close"
+            class="btn variant-ghost-surface"
+            onclick={drawerStore.close}
+    >
+      <CloseOutline/>
+    </button>
+    <header class="text-xl font-semibold">
+      {#if form.id}
+        <h2 class="h3">Edit Club "{form?.name}"</h2>
+      {:else}
+        <h2 class="h3">Create new Club</h2>
+      {/if}
+    </header>
+  </div>
 
-    <form class="mt-4 space-y-3" onsubmit={submitForm}>
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-3 xl:gap-4">
-            <input
-                    autocomplete="off"
-                    bind:value={form.id}
-                    class="input"
-                    name="id"
-                    readonly
-                    type="hidden"
-            />
+  <form class="mt-4 space-y-3" onsubmit={submitForm}>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-3 xl:gap-4">
+      <input
+              autocomplete="off"
+              bind:value={form.id}
+              class="input"
+              name="id"
+              readonly
+              type="hidden"
+      />
 
-            <label class="label">
-                <span>Name</span>
-                <input
-                        bind:value={form.name}
-                        class="input"
-                        name="name"
-                        required
-                        type="text"
-                />
-            </label>
+      <label class="label">
+        <span>Name</span>
+        <input
+                bind:value={form.name}
+                class="input"
+                name="name"
+                required
+                type="text"
+        />
+      </label>
 
-            <label class="label">
+      <label class="label">
                 <span>
                 Acronym
                 </span>
-                <input
-                        bind:value={form.acronym}
-                        class="input"
-                        name="acronym"
-                        type="text"
-                />
-            </label>
+        <input
+                bind:value={form.acronym}
+                class="input"
+                name="acronym"
+                type="text"
+        />
+      </label>
 
-            <label class="label">
+      <label class="label">
                 <span>
                 BSM Club ID
                 </span>
-                <input
-                        bind:value={form.bsm_id}
-                        class="input"
-                        name="bsm_id"
-                        required
-                        type="number"
-                >
-                <span class="text-sm">Can be found in the BSM address bar while editing
+        <input
+                bind:value={form.bsm_id}
+                class="input"
+                name="bsm_id"
+                required
+                type="number"
+        >
+        <span class="text-sm">Can be found in the BSM address bar while editing
                     (e.g. <span class="italic">https://bsm.baseball-softball.de/clubs/xxx/edit</span>).
                     Needs to be set for the app to function properly.
                 </span>
-            </label>
+      </label>
 
-            <label class="label">
+      <label class="label">
                 <span>
                 BSM API Key
                 </span>
-                <input
-                        bind:value={form.bsm_api_key}
-                        class="input"
-                        name="bsm_api_key"
-                        type="text"
-                />
-                <span class="text-sm">
+        <input
+                bind:value={form.bsm_api_key}
+                class="input"
+                name="bsm_api_key"
+                type="text"
+        />
+        <span class="text-sm">
                     Must be created in BSM in a user's account that has the role "Team Administration".
                     If set, all game events for the club can be automatically imported.
                 </span>
-            </label>
+      </label>
 
-            <label class="label">
+      <label class="label">
                 <span>
                 Signup Key
                 </span>
-                <input
-                        bind:value={form.signup_key}
-                        class="input"
-                        name="signup_key"
-                        placeholder="minimum 8 characters"
-                        minlength="8"
-                        required
-                        type="text"
-                />
-                <span class="text-sm">
+        <input
+                bind:value={form.signup_key}
+                class="input"
+                name="signup_key"
+                placeholder="minimum 8 characters"
+                minlength="8"
+                required
+                type="text"
+        />
+        <span class="text-sm">
                     A valid signup key needs to be entered upon user account creation.
                     New users are automatically added as members to the club corresponding to the signup key used.
                 </span>
-            </label>
+      </label>
 
-            {#if form.id /* we are editing and might want to change admins */}
-                <label class="label space-y-3 md:col-span-2">
-                    <span>Club Admins</span><br>
+      {#if form.id /* we are editing and might want to change admins */}
+        <label class="label space-y-3 md:col-span-2">
+          <span>Club Admins</span><br>
 
-                    {#each selectedAdmins as admin}
-                        <button
-                                type="button"
-                                class="chip variant-filled-primary me-1 lg:me-2"
-                                onclick={() => removeAdminFromSelection(admin)}
-                        >
-                            <span>{admin.first_name} {admin.last_name}</span>
-                            <CloseOutline size="xs"/>
-                        </button>
-                    {/each}
+          {#each selectedAdmins as admin}
+            <button
+                    type="button"
+                    class="chip variant-filled-primary me-1 lg:me-2"
+                    onclick={() => removeAdminFromSelection(admin)}
+            >
+              <span>{admin.first_name} {admin.last_name}</span>
+              <CloseOutline size="xs"/>
+            </button>
+          {/each}
 
-                    {#await allUsersForClub then users}
-                        <div>Select to add as admin:</div>
-                        <select
-                                class="select"
-                                bind:this={adminSelect}
-                                onchange={() => addAdminToSelection(users)}
-                        >
-                            <option selected value="">None</option>
-                            {#each users as user}
-                                <option value={user.id}>{user.first_name} {user.last_name}</option>
-                            {/each}
-                        </select>
-                    {/await}
+          {#await allUsersForClub then users}
+            <div>Select to add as admin:</div>
+            <select
+                    class="select"
+                    bind:this={adminSelect}
+                    onchange={() => addAdminToSelection(users)}
+            >
+              <option selected value="">None</option>
+              {#each users as user}
+                <option value={user.id}>{user.first_name} {user.last_name}</option>
+              {/each}
+            </select>
+          {/await}
 
-                    <span class="text-sm">
+          <span class="text-sm">
                         It is not possible to remove the last admin from a club.
                         <br>
                         Caution: You can remove yourself as admin, losing all access rights!
                     </span>
-                </label>
-            {/if}
-        </div>
+        </label>
+      {/if}
+    </div>
 
-        <hr class="!my-5"/>
+    <hr class="!my-5"/>
 
-        <div class="flex justify-center gap-3">
-            <button class="mt-2 btn variant-ghost-primary" type="submit">
-                Submit
-            </button>
-        </div>
-    </form>
+    <div class="flex justify-center gap-3">
+      <button class="mt-2 btn variant-ghost-primary" type="submit">
+        Submit
+      </button>
+    </div>
+  </form>
 </article>
