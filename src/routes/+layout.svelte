35df3f14ce08991cf94566c5346fe22ad4b289cf
David Battefeld <davidbattefeld@gmail.com>
2025-01-26 15:59:59 +0100

    refactor: use media query strategy for dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,13 @@
   import '../app.postcss';
   import {
     AppBar,
+    autoModeWatcher,
     Drawer,
     getDrawerStore,
     initializeStores,
-    LightSwitch,
     Modal,
     type ModalComponent,
-    Toast
+    Toast,
   } from "@skeletonlabs/skeleton";
   import Navigation from "$lib/components/meta/Navigation.svelte";
   import Footer from "$lib/components/meta/Footer.svelte";
@@ -16,7 +16,6 @@
   import LoginForm from "$lib/auth/LoginForm.svelte";
   import AccountModal from "$lib/auth/AccountModal.svelte";
   import {env} from "$env/dynamic/public"
-  import {onMount} from "svelte";
   import EventForm from '$lib/components/forms/EventForm.svelte';
   import TeamForm from '$lib/components/forms/TeamForm.svelte';
   import PlayerDataForm from '$lib/components/forms/PlayerDataForm.svelte';
@@ -49,44 +48,38 @@
     })
   }
 
-  // LightSwitch Workaround: https://github.com/skeletonlabs/skeleton/issues/2598
-  onMount(() => {
-    const e = document.documentElement.classList, t = localStorage.getItem("modeUserPrefers") === "false",
-        n = !("modeUserPrefers" in localStorage), r = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    t || n && r ? e.add("dark") : e.remove("dark")
-  })
-
   let showSidebar = $derived(data.clubs.length > 0 || data.teams.length > 0)
 </script>
 
 <svelte:head>
-    <title>Berlin Skylarks Web App</title>
+  <title>Berlin Skylarks Web App</title>
+  {@html '<script>(' + autoModeWatcher.toString() + ')();</script>'}
 </svelte:head>
 
 <Drawer>
-    {#if $drawerStore.id === "nav"}
+  {#if $drawerStore.id === "nav"}
 
-        <div class="flex justify-around p-2">
-            <img class="max-w-14" src="/berlin_skylarks_logo.svg" alt="Skylarks Team Logo">
+    <div class="flex justify-around p-2">
+      <img class="max-w-14" src="/berlin_skylarks_logo.svg" alt="Skylarks Team Logo">
 
-            <h2 class="p-4 antialiased">Berlin Skylarks</h2>
-        </div>
+      <h2 class="p-4 antialiased">Berlin Skylarks</h2>
+    </div>
 
-        <hr class="mb-2"/>
+    <hr class="mb-2"/>
 
-        <Navigation clubs={data.clubs} teams={data.teams}/>
+    <Navigation clubs={data.clubs} teams={data.teams}/>
 
-    {:else if $drawerStore.id === "event-form"}
-        <EventForm/>
-    {:else if $drawerStore.id === "team-form"}
-        <TeamForm/>
-    {:else if $drawerStore.id === "player-data-form"}
-        <PlayerDataForm/>
-    {:else if $drawerStore.id === "club-form"}
-        <ClubForm/>
-    {:else if $drawerStore.id === "eventseries-view"}
-        <EventSeriesView/>
-    {/if}
+  {:else if $drawerStore.id === "event-form"}
+    <EventForm/>
+  {:else if $drawerStore.id === "team-form"}
+    <TeamForm/>
+  {:else if $drawerStore.id === "player-data-form"}
+    <PlayerDataForm/>
+  {:else if $drawerStore.id === "club-form"}
+    <ClubForm/>
+  {:else if $drawerStore.id === "eventseries-view"}
+    <EventSeriesView/>
+  {/if}
 </Drawer>
 
 <!--Singletons-->
@@ -94,19 +87,19 @@
 <Toast/>
 
 <div class="h-screen grid grid-rows-[auto_1fr_auto]">
-    <!-- Header -->
-    <header>
+  <!-- Header -->
+  <header>
 
-        <AppBar
-                gridColumns="grid-cols-6"
-                slotDefault="place-self-center place-content-between w-full col-span-4"
-                slotTrail="place-content-end"
-                padding="p-3"
-                background="bg-surface-500/5"
-        >
-            <svelte:fragment slot="lead">
-                <div class="flex items-center justify-content-start">
-                    <button aria-label="open navigation" class="md:hidden btn btn-sm mr-4" onclick={navDrawerOpen}>
+    <AppBar
+            gridColumns="grid-cols-6"
+            slotDefault="place-self-center place-content-between w-full col-span-4"
+            slotTrail="place-content-end"
+            padding="p-3"
+            background="bg-surface-500/5"
+    >
+      <svelte:fragment slot="lead">
+        <div class="flex items-center justify-content-start">
+          <button aria-label="open navigation" class="md:hidden btn btn-sm mr-4" onclick={navDrawerOpen}>
                     <span>
                         <svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
                             <rect width="100" height="20"/>
@@ -114,63 +107,61 @@
                             <rect y="60" width="100" height="20"/>
                         </svg>
                     </span>
-                    </button>
+          </button>
 
-                    <a aria-label="to home page" href="/" class="hidden md:block ms-3">
-                        <img class="min-w-16" src="/berlin_skylarks_logo.svg" alt="Skylarks Team Logo">
-                    </a>
-                </div>
-            </svelte:fragment>
+          <a aria-label="to home page" href="/" class="hidden md:block ms-3">
+            <img class="min-w-16" src="/berlin_skylarks_logo.svg" alt="Skylarks Team Logo">
+          </a>
+        </div>
+      </svelte:fragment>
 
-            <svelte:fragment slot="default">
-                <section class="">
-                    <ul class="w-full justify-center items-center hidden lg:flex py-2 gap-2 xl:gap-16">
-                        <StaticNavigationLinks/>
-                    </ul>
-                </section>
-            </svelte:fragment>
+      <svelte:fragment slot="default">
+        <section class="">
+          <ul class="w-full justify-center items-center hidden lg:flex py-2 gap-2 xl:gap-16">
+            <StaticNavigationLinks/>
+          </ul>
+        </section>
+      </svelte:fragment>
 
-            <svelte:fragment slot="trail">
-                <div class="lg:me-5 flex items-center gap-5 flex-shrink-0">
+      <svelte:fragment slot="trail">
+        <div class="lg:me-5 flex items-center gap-5 flex-shrink-0">
 
-                    <LightSwitch/>
+          {#if env.PUBLIC_AUTH_FUNCS_ENABLED === "true"}
+            <LoginBadge signupAllowed={true}/>
+          {/if}
 
-                    {#if env.PUBLIC_AUTH_FUNCS_ENABLED === "true"}
-                        <LoginBadge signupAllowed={true}/>
-                    {/if}
+        </div>
+      </svelte:fragment>
 
-                </div>
-            </svelte:fragment>
+    </AppBar>
 
-        </AppBar>
+    <hr class="!border-b-2">
+  </header>
 
-        <hr class="!border-b-2">
-    </header>
+  <!-- Grid Column -->
 
-    <!-- Grid Column -->
+  <div class="grid grid-cols-1 md:grid-cols-[auto_1fr]">
 
-    <div class="grid grid-cols-1 md:grid-cols-[auto_1fr]">
+    <!-- Sidebar (Left) -->
+    {#if showSidebar}
+      <aside class="bg-surface-500/5 p-2 sticky top-0 col-span-1 hidden h-screen md:block w-64 lg:w-72 xl:w-80">
+        <Navigation clubs={data.clubs} teams={data.teams}/>
+      </aside>
+    {:else}
+      <!-- hack: render empty div to not mess up the grid -->
+      <div aria-hidden="true" class="hidden md:block"></div>
+    {/if}
 
-        <!-- Sidebar (Left) -->
-        {#if showSidebar}
-            <aside class="bg-surface-500/5 p-2 sticky top-0 col-span-1 hidden h-screen md:block w-64 lg:w-72 xl:w-80">
-                <Navigation clubs={data.clubs} teams={data.teams}/>
-            </aside>
-        {:else}
-            <!-- hack: render empty div to not mess up the grid -->
-            <div aria-hidden="true" class="hidden md:block"></div>
-        {/if}
+    <!-- Main -->
 
-        <!-- Main -->
+    <main class="col-span-1 space-y-4 lg:space-y-6 w-[93%] md:w-[90%] lg:w-[85%] justify-self-center">
+      {@render children?.()}
+    </main>
+  </div>
 
-        <main class="col-span-1 space-y-4 lg:space-y-6 w-[93%] md:w-[90%] lg:w-[85%] justify-self-center">
-            {@render children?.()}
-        </main>
-    </div>
-
-    <!-- Footer -->
-    <footer>
-        <hr class="!border-t-2">
-        <Footer></Footer>
-    </footer>
+  <!-- Footer -->
+  <footer>
+    <hr class="!border-t-2">
+    <Footer></Footer>
+  </footer>
 </div>
