8128ecb2f6a632d5b1746d6977c2be759d98cff9
David Battefeld <davidbattefeld@gmail.com>
2025-02-01 18:19:51 +0100

    style: indentation changes, typo

--- src/routes/teams/+page.svelte
+++ src/routes/teams/+page.svelte
@@ -5,38 +5,38 @@
     import {preferences} from "$lib/stores";
 
     interface Props {
-        data: any;
-    }
+    data: any;
+  }
 
-    let { data }: Props = $props();
+  let {data}: Props = $props();
 </script>
 
 <div class="my-2 md:flex justify-between items-center">
-    <h1 class="h1 mb-3">Teams</h1>
-    <div>
-        <SeasonSelector/>
-    </div>
+  <h1 class="h1 mb-3">Teams</h1>
+  <div>
+    <SeasonSelector/>
+  </div>
 </div>
 
 <ReloadUponSeasonChange/>
 
 {#await data.clubTeams}
-    <p>Lade Teams...</p>
-    <ProgressBar/>
+  <p>Loading Teams...</p>
+  <ProgressBar/>
 
 {:then clubTeams}
 
-    <ul class="list mt-5 flex flex-col gap-3">
-        {#each clubTeams ?? [] as clubTeam}
-            <li class="variant-soft-surface p-3 min-h-14">
-                <a href="/teams/{clubTeam.id}?season={$preferences.selectedSeason}">
-                    <span class="badge variant-ghost-primary w-20">{clubTeam.team.league_entries[0].league.acronym}</span>
-                    <span class="flex-auto ms-3">{clubTeam.team.name}</span>
-                </a>
-            </li>
-        {/each}
-    </ul>
+  <ul class="list mt-5 flex flex-col gap-3">
+    {#each clubTeams ?? [] as clubTeam}
+      <li class="variant-soft-surface p-3 min-h-14">
+        <a href="/teams/{clubTeam.id}?season={$preferences.selectedSeason}">
+          <span class="badge variant-ghost-primary w-20">{clubTeam.team.league_entries[0].league.acronym}</span>
+          <span class="flex-auto ms-3">{clubTeam.team.name}</span>
+        </a>
+      </li>
+    {/each}
+  </ul>
 
 {:catch error}
-    <p>error loading: {error.message}</p>
+  <p>error loading: {error.message}</p>
 {/await}
