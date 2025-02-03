6e92cb5c6cdf908f732815ba32368e0f9f420020
David Battefeld <davidbattefeld@gmail.com>
2025-02-03 21:18:56 +0100

    feat: add ballpark route

---
+++ src/lib/components/club/ballpark/BallparkGrid.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type {Field} from "bsm.js";
+
+  interface Props {
+    fields: Field[],
+  }
+
+  let {fields}: Props = $props();
+</script>
+
+<div class="grid grid-cols-1 md:grid-cols-2 gap-3 md:gap-4">
+  {#each fields as field}
+    <article
+            class="card p-3 variant-soft dark:border dark:border-surface-500 shadow-xl flex items-center gap-4">
+      <img class="rounded-token" width="80" height="80" loading="lazy" src="{field.photo_url}"
+           alt="a baseball or softball field: {field.name}">
+      <p>
+        {field.name}
+      </p>
+    </article>
+  {:else}
+    <p>No ballparks found.</p>
+  {/each}
+</div>
--- src/routes/club/ballpark/+page.svelte
+++ src/routes/club/ballpark/+page.svelte
@@ -1,1 +1,20 @@
-<p>Page</p>
+<script lang="ts">
+  import type {PageProps} from './$types';
+  import {ProgressBar} from "@skeletonlabs/skeleton";
+  import BallparkGrid from "$lib/components/club/ballpark/BallparkGrid.svelte";
+
+  let {data}: PageProps = $props();
+</script>
+
+<h1 class="h1">Ballpark</h1>
+
+
+{#await data.fields}
+  <p>Loading Club Officials...</p>
+  <ProgressBar/>
+
+{:then fields}
+  <BallparkGrid fields={fields}/>
+{:catch error}
+  <p>error loading: {error.message}</p>
+{/await}
