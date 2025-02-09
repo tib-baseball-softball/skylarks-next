195ba7ef82b5c2290e775eee4d20c6ac0839130c
David Battefeld <davidbattefeld@gmail.com>
2025-02-09 17:28:14 +0100

    fix: add fallback load for LeagueGroup in stats route

--- src/routes/ligen/[id]/stats/+page.ts
+++ src/routes/ligen/[id]/stats/+page.ts
@@ -2,27 +2,39 @@ import {type LeagueGroup} from "bsm.js";
 import {error} from "@sveltejs/kit";
 import {client} from "$lib/pocketbase/index.svelte";
 import type {LeaderboardSummary} from "$lib/model/LeaderboardData";
+import {env} from "$env/dynamic/public";
 
 /**
  * Gets statistics for league leaders in all relevant categories.
  */
-export async function load({parent, params, url}) {
-  const data = await parent()
-  const leagueGroups = await data.leagueGroups
-  let leagueGroup: LeagueGroup | undefined = leagueGroups.find((leagueGroup) => leagueGroup.id === Number(params.id))
+export async function load({parent, params, url, fetch}) {
+  const data = await parent();
+  const leagueGroups = await data.leagueGroups;
+  let leagueGroup: LeagueGroup | undefined = leagueGroups.find((leagueGroup) => leagueGroup.id === Number(params.id));
 
   if (!leagueGroup) {
-    throw error(404, "leagueGroup not found")
+    leagueGroup = await client.send<LeagueGroup>("/api/bsm/relay", {
+      fetch: fetch,
+      query: {
+        url: `https://bsm.baseball-softball.de/league_groups/${params.id}.json`,
+        club: env.PUBLIC_CLUB_ID,
+      },
+      requestKey: "leagueGroupStatsRoute"
+    });
+  }
+
+  if (!leagueGroup) {
+    throw error(404, "leagueGroup not found");
   }
 
   const leaderboardData = client.send<LeaderboardSummary>(`api/bsm/relay/top10/${leagueGroup.id}`, {
     query: {
       statsType: url.searchParams.get("statsType") ?? "",
     }
-  })
+  });
 
   return {
     leagueGroup: leagueGroup,
     leaderboardData: leaderboardData,
-  }
+  };
 }
