67d2ad93bb74f818bdd9124a1059421937c39885
David Battefeld <davidbattefeld@gmail.com>
2025-02-09 18:45:05 +0100

    feat: remove animations for events
    more distracting than dynamic in practice

--- src/routes/(protected)/account/team/[id]/+page.svelte
+++ src/routes/(protected)/account/team/[id]/+page.svelte
@@ -8,8 +8,6 @@
   import {RadioGroup, RadioItem,} from "@skeletonlabs/skeleton";
   import {authSettings} from "$lib/pocketbase/index.svelte";
   import TeamAdminSection from "$lib/components/diamondplanner/team/TeamAdminSection.svelte";
-  import {cubicOut} from "svelte/easing";
-  import {fly} from "svelte/transition";
 
   let {data} = $props();
   const events = $derived(data.events);
@@ -32,20 +30,20 @@
     reloadWithQuery();
   });
 
-  const authModel = authSettings.record as CustomAuthModel
+  const authModel = authSettings.record as CustomAuthModel;
 </script>
 
 <h1 class="h1">{data.team.name} ({data.team?.expand?.club.name})</h1>
 
 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mb-3">
-    <article class="card variant-soft-surface lg:col-span-2">
-        <header class="card-header">
-            <h2 class="h4 font-medium">Team Description</h2>
-        </header>
-        <section class="p-4">{@html data.team.description}</section>
-    </article>
+  <article class="card variant-soft-surface lg:col-span-2">
+    <header class="card-header">
+      <h2 class="h4 font-medium">Team Description</h2>
+    </header>
+    <section class="p-4">{@html data.team.description}</section>
+  </article>
 
-    <TeamTeaserCard team={data.team} link={false}/>
+  <TeamTeaserCard team={data.team} link={false}/>
 </div>
 
 <h2 class="h3">Team Events</h2>
@@ -53,77 +51,75 @@
 <div
         class="flex flex-wrap gap-4 lg:gap-8 variant-soft-surface justify-between px-4 py-3 rounded-token"
 >
-    <label
-            class="flex items-center gap-2 flex-grow justify-between md:flex-grow-0"
-    >
-        Timeframe
-        <RadioGroup>
-            <RadioItem
-                    checked
-                    name="radio-next"
-                    value="next"
-                    bind:group={showEvents}
-            >
-                Next
-            </RadioItem>
-            <RadioItem name="radio-past" value="past" bind:group={showEvents}>
-                Past
-            </RadioItem>
-        </RadioGroup>
-    </label>
+  <label
+          class="flex items-center gap-2 flex-grow justify-between md:flex-grow-0"
+  >
+    Timeframe
+    <RadioGroup>
+      <RadioItem
+              checked
+              name="radio-next"
+              value="next"
+              bind:group={showEvents}
+      >
+        Next
+      </RadioItem>
+      <RadioItem name="radio-past" value="past" bind:group={showEvents}>
+        Past
+      </RadioItem>
+    </RadioGroup>
+  </label>
 
-    <label class="label flex items-center gap-2 flex-grow justify-between md:flex-grow-0">
-        Sort
-        <RadioGroup>
-            <RadioItem checked name="radio-asc" value="asc" bind:group={sorting}>Ascending</RadioItem>
-            <RadioItem checked name="radio-desc" value="desc" bind:group={sorting}>Descending</RadioItem>
-        </RadioGroup>
-    </label>
+  <label class="label flex items-center gap-2 flex-grow justify-between md:flex-grow-0">
+    Sort
+    <RadioGroup>
+      <RadioItem checked name="radio-asc" value="asc" bind:group={sorting}>Ascending</RadioItem>
+      <RadioItem checked name="radio-desc" value="desc" bind:group={sorting}>Descending</RadioItem>
+    </RadioGroup>
+  </label>
 
-    <label class="label flex items-center gap-2 flex-grow justify-between md:flex-grow-0">
-        Type
-        <RadioGroup padding="px-2 md:px-4 py-1">
-            <RadioItem padding="px-4 py-1" checked name="radio-any" value="any" bind:group={showTypes}>All</RadioItem>
-            <RadioItem checked name="radio-game" value="game" bind:group={showTypes}>Game</RadioItem>
-            <RadioItem checked name="radio-practice" value="practice" bind:group={showTypes}>Practice</RadioItem>
-            <RadioItem checked name="radio-misc" value="misc" bind:group={showTypes}>Other</RadioItem>
-        </RadioGroup>
-    </label>
+  <label class="label flex items-center gap-2 flex-grow justify-between md:flex-grow-0">
+    Type
+    <RadioGroup padding="px-2 md:px-4 py-1">
+      <RadioItem padding="px-4 py-1" checked name="radio-any" value="any" bind:group={showTypes}>All</RadioItem>
+      <RadioItem checked name="radio-game" value="game" bind:group={showTypes}>Game</RadioItem>
+      <RadioItem checked name="radio-practice" value="practice" bind:group={showTypes}>Practice</RadioItem>
+      <RadioItem checked name="radio-misc" value="misc" bind:group={showTypes}>Other</RadioItem>
+    </RadioGroup>
+  </label>
 </div>
 
 <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 lg:gap-4 2xl:gap-5">
-    {#each $events.items as event, index}
-        {#key $events.items}
-            <div in:fly|global={{ y: 50, duration: index*300 + 100, delay: 200, easing: cubicOut }}>
-                <EventTeaser {event} link={true}/>
-            </div>
-        {/key}
-    {:else}
-        <p>No events available with the current filters.</p>
-    {/each}
+  {#each $events.items as event}
+    <div>
+      <EventTeaser {event} link={true}/>
+    </div>
+  {:else}
+    <p>No events available with the current filters.</p>
+  {/each}
 </div>
 
 <Paginator store={events} showIfSinglePage={false}/>
 
 <hr class="!my-8"/>
 <section class="space-y-2 lg:space-y-4">
-    <header>
-        <h2 class="h3">Links</h2>
-    </header>
+  <header>
+    <h2 class="h3">Links</h2>
+  </header>
 
-    <div class="flex flex-wrap items-center gap-2 lg:gap-3">
-        <a
-                href="/account/team/{data.team.id}/members"
-                class="btn variant-ghost-tertiary"
-        >
-            <UsersGroupOutline/>
-            <span>Player List</span>
-        </a>
-    </div>
+  <div class="flex flex-wrap items-center gap-2 lg:gap-3">
+    <a
+            href="/account/team/{data.team.id}/members"
+            class="btn variant-ghost-tertiary"
+    >
+      <UsersGroupOutline/>
+      <span>Player List</span>
+    </a>
+  </div>
 </section>
 
 {#if data.team.admins.includes(authModel?.id) || data.team?.expand?.club?.admins.includes(authModel?.id)}
-    <hr class="my-2"/>
+  <hr class="my-2"/>
 
-    <TeamAdminSection team={data.team} eventSeries={data.eventSeries}/>
+  <TeamAdminSection team={data.team} eventSeries={data.eventSeries}/>
 {/if}
