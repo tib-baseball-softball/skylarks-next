7757d02f98f65f1bd52539405703b48a21bade59
David Battefeld <davidbattefeld@gmail.com>
2025-02-19 19:14:44 +0100

    style: reformat GamecenterMatchSection

--- src/lib/components/match/GamecenterMatchSection.svelte
+++ src/lib/components/match/GamecenterMatchSection.svelte
@@ -10,34 +10,36 @@
   }
 
   let {matches = [], showExternal = false}: Props = $props();
-  let skylarksGames = matches.filter(match => match.away_team_name.includes(env.PUBLIC_TEAM_NAME) || match.home_team_name.includes(env.PUBLIC_TEAM_NAME))
+  let skylarksGames = matches.filter(
+      (match) =>
+          match.away_team_name.includes(env.PUBLIC_TEAM_NAME) ||
+          match.home_team_name.includes(env.PUBLIC_TEAM_NAME),
+  );
 </script>
 
 <section>
-    <header>
-        <h2 class="h2 my-3">Spiele</h2>
-    </header>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-6 mb-6">
-        {#if showExternal}
+  <header>
+    <h2 class="h2 my-3">Games</h2>
+  </header>
+  <div
+          class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-6 mb-6"
+  >
+    {#if showExternal}
+      {#each matches as match}
+        <MatchTeaserCard {match}/>
+      {/each}
 
-            {#each matches as match}
-                <MatchTeaserCard {match}/>
-            {/each}
+      {#if matches.length === 0}
+        <ContentFilteredUnavailable/>
+      {/if}
+    {:else}
+      {#each skylarksGames as match}
+        <MatchTeaserCard {match}/>
+      {/each}
 
-            {#if matches.length === 0}
-                <ContentFilteredUnavailable/>
-            {/if}
-
-        {:else}
-
-            {#each skylarksGames as match}
-                <MatchTeaserCard {match}/>
-            {/each}
-
-            {#if skylarksGames.length === 0}
-                <ContentFilteredUnavailable/>
-            {/if}
-
-        {/if}
-    </div>
+      {#if skylarksGames.length === 0}
+        <ContentFilteredUnavailable/>
+      {/if}
+    {/if}
+  </div>
 </section>
