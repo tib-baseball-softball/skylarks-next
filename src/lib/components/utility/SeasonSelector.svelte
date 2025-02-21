212d1df44320f618ad7e58b0720859f953c0b2b5
David Battefeld <davidbattefeld@gmail.com>
2025-02-21 16:48:05 +0100

    refactor: simplify SeasonSelector

--- src/lib/components/utility/SeasonSelector.svelte
+++ src/lib/components/utility/SeasonSelector.svelte
@@ -1,23 +1,12 @@
 <script lang="ts">
-  import {range} from "$lib/functions/range";
-  import {preferences} from "$lib/stores";
-  import type {AppPreferences} from "$lib/types/AppPreferences";
+    import {range} from "$lib/functions/range";
+    import {preferences} from "$lib/stores";
 
-  const seasonOptions = range(2021, new Date().getFullYear())
-
-  let selectedSeason: number = $state($preferences.selectedSeason)
-
-  function updatePreferences(event: Event) {
-    const target = event.target as HTMLSelectElement;
-    preferences.update((current: AppPreferences) => ({
-      ...current,
-      selectedSeason: parseInt(target?.value, 10)
-    }));
-  }
+    const seasonOptions = range(2021, new Date().getFullYear());
 </script>
 
-<select class="select" bind:value={selectedSeason} onchange={updatePreferences}>
-    {#each seasonOptions as option}
-        <option value="{option}">{option}</option>
-    {/each}
+<select class="select" bind:value={$preferences.selectedSeason}>
+  {#each seasonOptions as option}
+    <option value="{option}">{option}</option>
+  {/each}
 </select>
