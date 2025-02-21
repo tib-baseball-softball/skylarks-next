91741aad984f781e4147fe541ea4fa318f0b6949
David Battefeld <davidbattefeld@gmail.com>
2025-02-21 16:45:29 +0100

    refactor: remove legacy API from LeagueFilter and simplify code drastically
    fixes #2

--- src/lib/components/utility/LeagueFilter.svelte
+++ src/lib/components/utility/LeagueFilter.svelte
@@ -1,39 +1,18 @@
 <script lang="ts">
-    import { run } from 'svelte/legacy';
+  import {preferences} from "$lib/stores";
+  import type {LeagueGroup} from "bsm.js";
 
-    import {preferences} from "$lib/stores";
-    import type {AppPreferences} from "$lib/types/AppPreferences";
-    import type {LeagueGroup} from "bsm.js";
+  interface Props {
+    leagueGroups?: LeagueGroup[];
+  }
 
-    interface Props {
-        leagueGroups?: LeagueGroup[];
-    }
-
-    let { leagueGroups = [] }: Props = $props();
-
-    let selectedID = $state(0)
-
-    run(() => {
-        preferences.subscribe((value: AppPreferences) => {
-            if (value.leagueGroupID !== selectedID) {
-                selectedID = value.leagueGroupID;
-            }
-        });
-    });
-
-    function updatePreferences(event: Event) {
-        //const target = event.target as HTMLSelectElement;
-        preferences.update((current: AppPreferences) => ({
-            ...current,
-            leagueGroupID: selectedID
-        }));
-    }
+  let {leagueGroups = []}: Props = $props();
 </script>
 
-<select class="select min-w-52" bind:value={selectedID} onchange={updatePreferences}>
-    <option value="{0}" selected>Alle Ligen</option>
+<select class="select min-w-52" bind:value={$preferences.leagueGroupID}>
+  <option value="{0}" selected>Alle Ligen</option>
 
-    {#each leagueGroups as leagueGroup}
-        <option value="{leagueGroup.id}">{leagueGroup.name}</option>
-    {/each}
+  {#each leagueGroups as leagueGroup}
+    <option value="{leagueGroup.id}">{leagueGroup.name}</option>
+  {/each}
 </select>
