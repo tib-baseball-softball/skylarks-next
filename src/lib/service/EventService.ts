5f04e89b3942f9aa45da48b53589d22eab16fcee
David Battefeld <davidbattefeld@gmail.com>
2025-02-22 09:08:29 +0100

    refactor: make EventService use the new Fetch type

--- src/lib/service/EventService.ts
+++ src/lib/service/EventService.ts
@@ -1,38 +1,36 @@
-import type { ExpandedEvent } from "$lib/model/ExpandedResponse";
-import type { PageStore } from "$lib/pocketbase/PageStore";
-import { watchWithPagination } from "$lib/pocketbase/RecordOperations";
+import type {ExpandedEvent} from "$lib/model/ExpandedResponse";
+import type {PageStore} from "$lib/pocketbase/PageStore";
+import {watchWithPagination} from "$lib/pocketbase/RecordOperations";
+import type {Fetch} from "$lib/types/Fetch.ts";
 
 export class EventService {
   /**
-  * Takes a team ID and an URL, construct PocketBase filter string from query parameters
-  * and loads events with realtime.
-  */
-  public async loadEventStore(teamID: string, url: URL, fetch: {
-    (input: RequestInfo | URL, init?: RequestInit): Promise<Response>;
-    (input: string | URL | globalThis.Request, init?: RequestInit): Promise<Response>;
-  }): Promise<PageStore<ExpandedEvent>> {
-    let filter = `team = "${teamID}"`
+   * Takes a team ID and an URL, construct PocketBase filter string from query parameters
+   * and loads events with realtime.
+   */
+  public async loadEventStore(teamID: string, url: URL, fetch: Fetch): Promise<PageStore<ExpandedEvent>> {
+    let filter = `team = "${teamID}"`;
 
-    let timeframe = url.searchParams.get("timeframe")
+    let timeframe = url.searchParams.get("timeframe");
 
     if (!timeframe || timeframe === "next") {
-      filter = filter.concat(`&& starttime >= @todayStart`)
+      filter = filter.concat(`&& starttime >= @todayStart`);
     } else if (timeframe === "past") {
-      filter = filter.concat(`&& starttime <= @todayStart`)
+      filter = filter.concat(`&& starttime <= @todayStart`);
     }
 
-    const showTypes = url.searchParams.get("type") ?? "any"
+    const showTypes = url.searchParams.get("type") ?? "any";
 
     if (showTypes !== "any") {
-      filter = filter.concat(`&& type = "${showTypes}"`)
+      filter = filter.concat(`&& type = "${showTypes}"`);
     }
 
     // add sort parameter
 
-    let sort = "+starttime"
+    let sort = "+starttime";
 
     if (url.searchParams.get("sort") === "desc") {
-      sort = "-starttime"
+      sort = "-starttime";
     }
 
     // check pagination info
@@ -40,15 +38,15 @@ export class EventService {
     const pageNumber = Number(url.searchParams.get("page")) ?? 1;
 
     return await watchWithPagination<ExpandedEvent>(
-      "events",
-      {
-        filter: filter,
-        sort: sort,
-        expand: "participations_via_event.user, attire",
-        fetch: fetch
-      },
-      pageNumber,
-      6,
+        "events",
+        {
+          filter: filter,
+          sort: sort,
+          expand: "participations_via_event.user, attire",
+          fetch: fetch
+        },
+        pageNumber,
+        6,
     );
   }
 }
