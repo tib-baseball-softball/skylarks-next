f945e7181ec44abb76d691fc3f575981512a5f9a
David Battefeld <davidbattefeld@gmail.com>
2025-02-23 09:28:05 +0100

    polish: link address of ballparks
    closes #38

--- src/lib/components/club/ballpark/BallparkDetailCard.svelte
+++ src/lib/components/club/ballpark/BallparkDetailCard.svelte
@@ -2,12 +2,15 @@
   import type {Field} from "bsm.js";
   import GenericDetailRow from "$lib/components/utility/GenericDetailRow.svelte";
   import {Diamond, Info, MapPin, Ticket, Users} from "lucide-svelte";
+  import {MapUtility} from "$lib/service/MapUtility.ts";
 
   interface Props {
     field: Field,
   }
 
   let {field}: Props = $props();
+
+  const mapsLink = $derived(MapUtility.buildGoogleMapsURL(field?.name, field?.latitude, field?.longitude));
 </script>
 
 <article class="card p-3 variant-soft dark:border dark:border-surface-500 shadow-xl">
@@ -45,24 +48,34 @@
 
   <hr class="my-2">
 
-  <GenericDetailRow
-          categoryName="Address"
-          rowValue="{field.street}, {field.postal_code} {field.city}"
-  >
-    {#snippet icon()}
-      <div class="tertiary">
-        <MapPin/>
-      </div>
-    {/snippet}
-  </GenericDetailRow>
+  <div class="container">
+    <div class="tertiary">
+      <MapPin/>
+    </div>
+    <dl class="container">
+      <dt><strong class="">Address:</strong></dt>
+      <dd>
+        <a class="anchor" href="{mapsLink}" target="_blank">
+          {field.street}, {field.postal_code} {field.city}
+        </a>
+      </dd>
+    </dl>
+  </div>
+
 </article>
 
-<style>
+<style lang="postcss">
     .primary {
         color: rgba(var(--color-primary-500));
     }
 
     .tertiary {
         color: light-dark(rgba(var(--color-secondary-500)), rgba(var(--color-tertiary-500)));
+    }
+
+    .container {
+        display: flex;
+        align-items: center;
+        @apply gap-4;
     }
 </style>
