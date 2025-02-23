0e64dc647953df7ef789698a428ca3a744c53307
David Battefeld <davidbattefeld@gmail.com>
2025-02-23 09:17:18 +0100

    chore: translate AccountModal

--- src/lib/auth/AccountModal.svelte
+++ src/lib/auth/AccountModal.svelte
@@ -15,40 +15,40 @@
     parent.onClose();
   }
 
-  const authModel = authSettings.record as CustomAuthModel
+  const authModel = authSettings.record as CustomAuthModel;
 </script>
 
 <div class="w-modal-slim">
-    <div class="card p-4">
-        <div class="flex mt-1 mb-5 justify-between items-center">
-            <p>Eingeloggt als:</p>
+  <div class="card p-4">
+    <div class="flex mt-1 mb-5 justify-between items-center">
+      <p>Logged in as:</p>
 
-            <a
-                    href="/account"
-                    class="badge variant-filled-primary"
-                    onclick={closeModal}
-            >
-                <samp
-                >{`${authModel?.first_name ?? ""} ${authModel?.last_name ?? ""}`}</samp
-                >
-            </a>
-        </div>
-
-        <div class="flex mt-1 mb-5 justify-between items-start">
-            <p>Club:</p>
-
-            <div class="flex flex-wrap gap-2 justify-end">
-                {#each authModel?.expand?.club as club}
-                    <a href="/account/clubs/{club.id}" class="badge variant-filled-tertiary" onclick={closeModal}>
-                        {club.name}
-                    </a>
-                {/each}
-            </div>
-        </div>
-
-        <button class="btn variant-ghost-primary mb-1" onclick={logout}
-        >Abmelden
-        </button
+      <a
+              href="/account"
+              class="badge variant-filled-primary"
+              onclick={closeModal}
+      >
+        <samp
+        >{`${authModel?.first_name ?? ""} ${authModel?.last_name ?? ""}`}</samp
         >
+      </a>
     </div>
+
+    <div class="flex mt-1 mb-5 justify-between items-start">
+      <p>Club:</p>
+
+      <div class="flex flex-wrap gap-2 justify-end">
+        {#each authModel?.expand?.club as club}
+          <a href="/account/clubs/{club.id}" class="badge variant-filled-tertiary" onclick={closeModal}>
+            {club.name}
+          </a>
+        {/each}
+      </div>
+    </div>
+
+    <button class="btn variant-ghost-primary mb-1" onclick={logout}
+    >Log out
+    </button
+    >
+  </div>
 </div>
