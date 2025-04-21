2349d19c0d0dc7b9c911bd2dd306c81c5f1351a2
David Battefeld <davidbattefeld@gmail.com>
2025-04-21 19:34:38 +0200

    chore: migrate to the Tailwind Vite Plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import {sveltekit} from '@sveltejs/kit/vite';
 import {defineConfig} from 'vite';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
     host: true
   },
-  plugins: [sveltekit()],
+  plugins: [
+      tailwindcss(),
+      sveltekit(),
+  ],
 });
