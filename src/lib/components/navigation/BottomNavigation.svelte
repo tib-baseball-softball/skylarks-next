1b828c12872b893a1c83ea0d946bf3c22376fb23
David Battefeld <davidbattefeld@gmail.com>
2025-06-07 11:45:34 +0200

    fix: account for iOS safe area in BottomNavigation.svelte

--- src/lib/components/navigation/BottomNavigation.svelte
+++ src/lib/components/navigation/BottomNavigation.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <nav
-    class="lg:hidden bottom-nav bg-surface-50 dark:bg-surface-800 border-t border-surface-500 dark:border-surface-100 z-2"
+    class="lg:hidden bottom-nav bg-surface-50 dark:bg-surface-800 border-t border-b border-surface-500 dark:border-surface-100 z-2"
 >
   <ul class="flex">
     <StaticNavigationLinks classes="flex-col py-1.5 text-sm"/>
@@ -16,5 +16,6 @@
         bottom: 0;
         left: 0;
         width: 100%;
+        padding-bottom: env(safe-area-inset-bottom);
     }
 </style>
