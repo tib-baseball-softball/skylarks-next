0a30bf8720c0fdee27c62045edbee66f330ae2da
David Battefeld <davidbattefeld@gmail.com>
2025-06-13 10:15:58 +0200

    test: add test for favorite team endpoint

--- backend/main_test.go
+++ backend/main_test.go
@@ -228,3 +228,60 @@ func TestGetUserStatsEndpoint(t *testing.T) {
 		scenario.Test(t)
 	}
 }
+
+func TestFavoriteTeamEndpoint(t *testing.T) {
+	scenarios := []tests.ApiScenario{
+		{
+			Name:            "try with different http method, e.g. POST",
+			Method:          http.MethodPost,
+			URL:             "/api/team/favorite",
+			ExpectedStatus:  http.StatusNotFound,
+			ExpectedContent: []string{"wasn't found"},
+			TestAppFactory:  setupTestApp,
+		},
+		{
+			Name:            "missing team parameter",
+			Method:          http.MethodGet,
+			URL:             "/api/team/favorite?season=2025",
+			ExpectedStatus:  http.StatusBadRequest,
+			ExpectedContent: []string{"Team ID not found in request"},
+			TestAppFactory:  setupTestApp,
+		},
+		{
+			Name:            "invalid team parameter",
+			Method:          http.MethodGet,
+			URL:             "/api/team/favorite?team=invalid&season=2025",
+			ExpectedStatus:  http.StatusBadRequest,
+			ExpectedContent: []string{"Team ID must be an integer"},
+			TestAppFactory:  setupTestApp,
+		},
+		{
+			Name:            "missing season parameter",
+			Method:          http.MethodGet,
+			URL:             "/api/team/favorite?team=23219",
+			ExpectedStatus:  http.StatusBadRequest,
+			ExpectedContent: []string{"Season not found in request"},
+			TestAppFactory:  setupTestApp,
+		},
+		{
+			Name:            "invalid season parameter",
+			Method:          http.MethodGet,
+			URL:             "/api/team/favorite?team=23219&season=invalid",
+			ExpectedStatus:  http.StatusBadRequest,
+			ExpectedContent: []string{"Season must be an integer"},
+			TestAppFactory:  setupTestApp,
+		},
+		{
+			Name:            "valid request",
+			Method:          http.MethodGet,
+			URL:             "/api/team/favorite?team=23219&season=2025",
+			ExpectedStatus:  http.StatusOK,
+			ExpectedContent: []string{"league_group", "playoff_series", "table", "table_row"},
+			TestAppFactory:  setupTestApp,
+		},
+	}
+
+	for _, scenario := range scenarios {
+		scenario.Test(t)
+	}
+}
