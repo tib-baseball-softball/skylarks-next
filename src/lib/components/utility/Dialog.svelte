5784b1a3e297b015b51c745451608af3d233c2ae
David Battefeld <davidbattefeld@gmail.com>
2025-07-04 15:26:05 +0200

    polish: increase dialog backdrop opacity

--- src/lib/components/utility/Dialog.svelte
+++ src/lib/components/utility/Dialog.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-  import type {Snippet} from "svelte";
-  import {Dialog, type WithoutChild} from "bits-ui";
-  import {fly} from "svelte/transition";
-  import {cubicInOut} from "svelte/easing";
-  import {X} from "lucide-svelte";
+  import type { Snippet } from "svelte";
+  import { Dialog, type WithoutChild } from "bits-ui";
+  import { fly } from "svelte/transition";
+  import { cubicInOut } from "svelte/easing";
+  import { X } from "lucide-svelte";
 
   /**
    * Modal dialog based on headless bits-ui building blocks.
-   * Potential replacement for Skeleton modal.
+   * Replacement for Skeleton modal.
    */
 
   type Props = Dialog.RootProps & {
@@ -35,29 +35,34 @@
 </script>
 
 <Dialog.Root bind:open {...restProps}>
-  <Dialog.Trigger class="{triggerClasses} flex gap-1" type="button" disabled={disabled}>
+  <Dialog.Trigger class="{triggerClasses} flex gap-1" type="button" {disabled}>
     {@render triggerContent()}
   </Dialog.Trigger>
   <Dialog.Portal>
     <Dialog.Overlay
-            class="fixed inset-0 z-50 bg-surface-50/50 dark:bg-surface-950/50"/>
-    <Dialog.Content forceMount
-                    class="card bg-surface-50 dark:bg-surface-800 border shadow-2xl fixed left-[50%] top-[50%] z-50 w-full max-w-[94%] translate-x-[-50%] translate-y-[-50%] p-5 sm:max-w-[640px] md:w-full"
-                    {...contentProps}>
-
-      {#snippet child({props, open})}
+      class="fixed inset-0 z-50 bg-surface-50/80 dark:bg-surface-950/80"
+    />
+    <Dialog.Content
+      forceMount
+      class="card bg-surface-50 dark:bg-surface-800 border shadow-2xl fixed left-[50%] top-[50%] z-50 w-full max-w-[94%] translate-x-[-50%] translate-y-[-50%] p-5 sm:max-w-[640px] md:w-full"
+      {...contentProps}
+    >
+      {#snippet child({ props, open })}
         {#if open}
-          <div {...props} transition:fly={{y: 150, duration: 100, easing: cubicInOut}}>
+          <div
+            {...props}
+            transition:fly={{ y: 150, duration: 100, easing: cubicInOut }}
+          >
             <div class="flex gap-5 items-center mb-2">
-
-              <Dialog.Close class="btn preset-tonal-surface border border-surface-500 {closeButtonClasses}">
-                <X/>
+              <Dialog.Close
+                class="btn preset-tonal-surface border border-surface-500 {closeButtonClasses}"
+              >
+                <X />
               </Dialog.Close>
 
               <Dialog.Title class="text-xl font-semibold">
                 {@render title()}
               </Dialog.Title>
-
             </div>
 
             {#if description}
@@ -70,8 +75,6 @@
           </div>
         {/if}
       {/snippet}
-
     </Dialog.Content>
   </Dialog.Portal>
 </Dialog.Root>
-
