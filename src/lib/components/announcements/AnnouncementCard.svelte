0e81f80a789e2785b6b317285b0517e49cd3d384
David Battefeld <davidbattefeld@gmail.com>
2025-07-11 08:43:48 +0200

    polish: make announcement teasers same height

--- src/lib/components/announcements/AnnouncementCard.svelte
+++ src/lib/components/announcements/AnnouncementCard.svelte
@@ -1,28 +1,29 @@
 <script lang="ts">
-  import type {
-    ExpandedAnnouncement,
-    ExpandedClub,
-  } from "$lib/model/ExpandedResponse";
-  import Dialog from "../utility/Dialog.svelte";
+  import type {ExpandedAnnouncement,} from "$lib/model/ExpandedResponse";
   import AnnouncementCoreContent from "./AnnouncementCoreContent.svelte";
-  import AnnouncementDialog from "../forms/AnnouncementForm.svelte";
 
   interface Props {
     announcement: ExpandedAnnouncement;
   }
 
-  let { announcement }: Props = $props();
+  let {announcement}: Props = $props();
 </script>
 
-<article class="card p-4 preset-tonal-surface shadow-xl">
-  <AnnouncementCoreContent {announcement} textClasses="line-clamp-5" />
+<article class="card p-4 preset-tonal-surface shadow-xl h-full">
+  <div class="flex flex-col justify-between h-full">
+    <div>
+      <AnnouncementCoreContent {announcement} textClasses="line-clamp-5"/>
+    </div>
 
-  <hr class="mt-6 mb-2" />
+    <div>
+      <hr class="mt-6 mb-2"/>
 
-  <footer class="mt-3 flex gap-2 justify-end">
-    <a
-      href="/account/announcements/{announcement.id}"
-      class="btn btn-sm preset-filled-primary-500">Read more</a
-    >
-  </footer>
+      <footer class="mt-3 flex gap-2 justify-end">
+        <a
+                href="/account/announcements/{announcement.id}"
+                class="btn btn-sm preset-filled-primary-500">Read more</a
+        >
+      </footer>
+    </div>
+  </div>
 </article>
