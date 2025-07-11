c59ac6d5de269e067bf0bd38e96402b1a65454a5
David Battefeld <davidbattefeld@gmail.com>
2025-07-11 09:16:16 +0200

    feat: update announcement detail page

--- src/routes/(protected)/account/announcements/[announcement]/+page.svelte
+++ src/routes/(protected)/account/announcements/[announcement]/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-  import type { PageProps } from "./$types";
-  import { User } from "lucide-svelte";
+  import type {PageProps} from "./$types";
+  import {User} from "lucide-svelte";
   import PriorityBadge from "$lib/components/announcements/PriorityBadge.svelte";
-  import type { CustomAuthModel } from "$lib/model/ExpandedResponse";
-  import { authSettings, client } from "$lib/pocketbase/index.svelte";
+  import type {CustomAuthModel} from "$lib/model/ExpandedResponse";
+  import {authSettings, client} from "$lib/pocketbase/index.svelte";
   import DeleteButton from "$lib/components/utility/DeleteButton.svelte";
 
-  let { data }: PageProps = $props();
+  let {data}: PageProps = $props();
   const announcement = $derived(data.announcement);
 
   const authRecord = $derived(authSettings.record as CustomAuthModel);
@@ -16,33 +16,59 @@
   }
 </script>
 
+<div class="flex justify-between items-center gap-4 lg:gap-6">
+  <h1 class="h1 my-3!">{$announcement.title}</h1>
+  <PriorityBadge priority={$announcement.priority}/>
+</div>
+
 <div class="flex justify-center">
   <div>
-    <div class="flex justify-between items-center gap-4 lg:gap-6">
-      <h1 class="h1 my-3!">{$announcement.title}</h1>
-      <PriorityBadge priority={$announcement.priority} />
-    </div>
-
-    <p class="my-6 flex gap-3 items-center">
-      <User size="28" />
-      <span class="h6">
+    <section>
+      <p class="my-6 flex gap-3 items-center">
+        <User size="28"/>
+        <span class="h6">
         {$announcement.expand?.author.first_name}
-        {$announcement.expand?.author.last_name}
+          {$announcement.expand?.author.last_name}
       </span>
-    </p>
+      </p>
+    </section>
 
-    <div class="prose text-justify">
+    <section class="prose text-justify">
       {@html $announcement.bodytext}
-    </div>
 
-    <div class="flex justify-end">
+    </section>
+
+    {#if $announcement.link}
+      <section class="my-4">
+        <header>
+          <h2 class="h4">Link</h2>
+        </header>
+
+        <a class="anchor block mt-2" href={$announcement.link} target="_blank"
+           title="Link associated with this announcement: {$announcement.link}">
+          {#if $announcement.link_text}
+            {$announcement.link_text}
+          {:else }
+            {$announcement.link}
+          {/if}
+        </a>
+      </section>
+    {/if}
+
+    <section class="my-4">
+
+      <header>
+        <h2 class="h4">Admin Options</h2>
+      </header>
       {#if $announcement?.expand?.club?.admins.includes(authRecord.id)}
-        <DeleteButton
-          id={$announcement.id}
-          modelName="Announcement"
-          action={deleteAction}
-        />
+        <div class="flex mt-3">
+          <DeleteButton
+                  id={$announcement.id}
+                  modelName="Announcement"
+                  action={deleteAction}
+          />
+        </div>
       {/if}
-    </div>
+    </section>
   </div>
 </div>
