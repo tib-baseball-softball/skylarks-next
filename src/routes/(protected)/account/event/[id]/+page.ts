5f91e1a749c9e81d91b526d9142f250e88b26281
David Battefeld <davidbattefeld@gmail.com>
2025-07-12 16:09:32 +0200

    feat: display comment section on event detail page

--- src/routes/(protected)/account/event/[id]/+page.ts
+++ src/routes/(protected)/account/event/[id]/+page.ts
@@ -1,14 +1,14 @@
-import { watchSingleRecord } from "$lib/pocketbase/RecordOperations";
-import type { ExpandedEvent } from "$lib/model/ExpandedResponse.js";
-import type { PageLoad } from "./$types";
+import {watchSingleRecord} from "$lib/pocketbase/RecordOperations";
+import type {ExpandedEvent} from "$lib/model/ExpandedResponse.js";
+import type {PageLoad} from "./$types";
 
 export const load = (async ({ fetch, params, depends }) => {
   const event = await watchSingleRecord<ExpandedEvent>("events", params.id, {
-    expand: "participations_via_event.user, attire, location, team.admins, team.club",
+    expand: "participations_via_event.user, attire, location, team.admins, team.club, comments_via_event.user",
     fetch: fetch
   })
 
-  depends("event:list")
+  depends("event:list", "comments:list")
 
   return {
     event: event
