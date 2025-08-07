aa848d7b96adc0cd8a48da4251094b9b252c0805
David Battefeld <davidbattefeld@gmail.com>
2025-08-07 21:13:13 +0200

    fix: correct grid on player profile

--- src/routes/(protected)/account/playerprofile/+page.svelte
+++ src/routes/(protected)/account/playerprofile/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-    import PlayerDataProfileSection from "$lib/components/diamondplanner/user/PlayerDataProfileSection.svelte";
+  import PlayerDataProfileSection from "$lib/components/diamondplanner/user/PlayerDataProfileSection.svelte";
 </script>
 
 <h1 class="h1 mt-4! mb-6!">My Player Data</h1>
 
-<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mb-3">
+<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 mb-3">
     <PlayerDataProfileSection />
 </div>
