17bc4894221b4b651f19cb8cd822c9a9e2ec9594
David Battefeld <post@davidbattefeld.de>
2025-08-29 13:04:26 +0200

    style: sort root load funcs

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,15 +1,25 @@
-import {ClubTeamsAPIRequest, LeagueGroupAPIRequest} from "bsm.js";
-import {env} from "$env/dynamic/private";
-import {preferences} from "$lib/globals.svelte.ts";
-import {env as publicEnv} from "$env/dynamic/public";
+import { ClubTeamsAPIRequest, LeagueGroupAPIRequest } from "bsm.js";
+import { env } from "$env/dynamic/private";
+import { preferences } from "$lib/globals.svelte.ts";
+import { env as publicEnv } from "$env/dynamic/public";
+import type { LayoutServerLoad } from "./$types";
 
-export const load = (async () => {
+export const load: LayoutServerLoad = async () => {
   const leagueGroupRequest = new LeagueGroupAPIRequest(env.BSM_API_KEY);
   const clubTeamRequest = new ClubTeamsAPIRequest(env.BSM_API_KEY);
   const appPreferences = preferences.current;
 
+  const clubTeams = clubTeamRequest.getTeamsForClub(
+    Number(publicEnv.PUBLIC_CLUB_ID),
+    appPreferences.selectedSeason,
+  );
+
+  const leagueGroups = leagueGroupRequest.getLeagueGroupsForClub(
+    appPreferences.selectedSeason,
+  );
+
   return {
-    clubTeams: clubTeamRequest.getTeamsForClub(Number(publicEnv.PUBLIC_CLUB_ID), appPreferences.selectedSeason),
-    leagueGroups: leagueGroupRequest.getLeagueGroupsForClub(appPreferences.selectedSeason)
+    clubTeams: clubTeams,
+    leagueGroups: leagueGroups,
   };
-})
+};
