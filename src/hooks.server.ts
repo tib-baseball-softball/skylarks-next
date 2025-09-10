2f45763ec808a4d769642b21630342dae42b4a82
David Battefeld <post@davidbattefeld.de>
2025-09-10 22:29:06 +0200

    fix: shuffle i18n files

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,11 @@
+import {loadLocales, runWithLocale} from "wuchale/load-utils/server";
+import type {Handle} from "@sveltejs/kit";
+import {key, loadCatalog, loadIDs} from './locales/loader.ssr.svelte.js';
+import {locales} from "virtual:wuchale/locales";
+
 /**
  * @see https://wuchale.dev/adapters/svelte/#sveltekit-ssrssg
  */
-import {loadLocales, runWithLocale} from "wuchale/load-utils/server";
-import type {Handle} from "@sveltejs/kit";
-import {key, loadCatalog, loadIDs} from "virtual:wuchale/proxy";
-import {locales} from "virtual:wuchale/locales";
 
 // load at server startup
 loadLocales(key, loadIDs, loadCatalog, locales);
