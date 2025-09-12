1499f7ed169bc6c25cb0d0de06a2a61b94a6cdf9
David Battefeld <post@davidbattefeld.de>
2025-09-12 23:57:28 +0200

    polish: remove context hint from translation

--- src/lib/components/meta/TopAppBarTrailing.svelte
+++ src/lib/components/meta/TopAppBarTrailing.svelte
@@ -4,6 +4,7 @@
   import LocalePicker from "$lib/components/meta/LocalePicker.svelte";
 </script>
 
+<!-- @wc-ignore -->
 {#if env.PUBLIC_APPLICATION_CONTEXT !== "Production"}
   <div class="preset-tonal-warning p-1.5 rounded-container border border-warning-500 text-sm">
     {env.PUBLIC_APPLICATION_CONTEXT}
