5f175ca97318a1e3bfce69c9c3486722e88c9a96
David Battefeld <post@davidbattefeld.de>
2025-10-31 16:44:20 +0100

    refactor: use native CSS for sheet overlay

--- src/lib/components/utility/sheet/sheet-overlay.svelte
+++ src/lib/components/utility/sheet/sheet-overlay.svelte
@@ -13,8 +13,30 @@
 <SheetPrimitive.Overlay
         {...restProps}
         bind:ref
-        class={[
-    "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-surface-50/50 dark:bg-surface-950/50 drawer-overlay",
-    className,
-  ]}
+        class={["drawer-overlay", className]}
 />
+
+<style>
+    :global {
+        .drawer-overlay {
+            position: fixed;
+            inset: 0;
+            z-index: 50;
+            background-color: light-dark(var(--color-surface-100), #0a0a0a80);
+
+            @media (prefers-color-scheme: light) {
+                opacity: 50%;
+            }
+
+            &[data-state="closed"] {
+                --tw-exit-opacity: 0;
+                animation: exit var(--tw-animation-duration, var(--tw-duration, .15s)) var(--tw-ease, ease) var(--tw-animation-delay, 0s) var(--tw-animation-iteration-count, 1) var(--tw-animation-direction, normal) var(--tw-animation-fill-mode, none);
+            }
+
+            &[data-state="open"] {
+                --tw-enter-opacity: 0;
+                animation: enter var(--tw-animation-duration, var(--tw-duration, .15s)) var(--tw-ease, ease) var(--tw-animation-delay, 0s) var(--tw-animation-iteration-count, 1) var(--tw-animation-direction, normal) var(--tw-animation-fill-mode, none);
+            }
+        }
+    }
+</style>
