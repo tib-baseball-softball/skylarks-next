2834aece4df5c14cc6ee33276097de12c8b06c80
David Battefeld <post@davidbattefeld.de>
2026-01-21 19:52:23 +0100

    feat: add team record proxy

---
+++ dp/team.go
@@ -0,0 +1,71 @@
+package dp
+
+import "github.com/pocketbase/pocketbase/core"
+
+const (
+	TeamsCollection = "teams"
+)
+
+var _ core.RecordProxy = (*Team)(nil)
+
+// Team RecordProxy for collection `teams`.
+// Provides type-safe struct access to team records.
+type Team struct {
+	core.BaseRecordProxy
+}
+
+func (t *Team) Name() string {
+	return t.GetString("name")
+}
+
+func (t *Team) SetName(name string) {
+	t.Set("name", name)
+}
+
+func (t *Team) BSMLeagueGroup() int {
+	return t.GetInt("bsm_league_group")
+}
+
+func (t *Team) SetBSMLeagueGroup(id int) {
+	t.Set("bsm_league_group", id)
+}
+
+func (t *Team) AgeGroup() string {
+	return t.GetString("age_group")
+}
+
+func (t *Team) SetAgeGroup(ageGroup string) {
+	t.Set("age_group", ageGroup)
+}
+
+func (t *Team) Club() string {
+	return t.GetString("club")
+}
+
+func (t *Team) SetClub(clubID string) {
+	t.Set("club", clubID)
+}
+
+func (t *Team) Description() string {
+	return t.GetString("description")
+}
+
+func (t *Team) SetDescription(desc string) {
+	t.Set("description", desc)
+}
+
+func (t *Team) Admins() []string {
+	return t.GetStringSlice("admins")
+}
+
+func (t *Team) SetAdmins(adminIDs []string) {
+	t.Set("admins", adminIDs)
+}
+
+func (t *Team) SignupKey() string {
+	return t.GetString("signup_key")
+}
+
+func (t *Team) SetSignupKey(key string) {
+	t.Set("signup_key", key)
+}
