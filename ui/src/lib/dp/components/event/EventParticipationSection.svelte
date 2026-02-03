2211579321380c1aabc1f22d80ad25f8f5f9ba36
David Battefeld <post@davidbattefeld.de>
2026-02-03 23:04:55 +0100

    feat: allow non-members to see participations in list view
    closes #100

--- ui/src/lib/dp/components/event/EventParticipationSection.svelte
+++ ui/src/lib/dp/components/event/EventParticipationSection.svelte
@@ -16,14 +16,16 @@
   const {event, chipClasses = ""}: props = $props();
 
   const userParticipation: ParticipationsCreate = $derived(
-      event.userParticipation ?? {
-        id: "",
-        user: authRecord?.id,
-        event: event.id,
-        state: "",
-        comment: "",
-      }
+    event.userParticipation ?? {
+      id: "",
+      user: authRecord?.id,
+      event: event.id,
+      state: "",
+      comment: "",
+    }
   );
+
+  const canParticipate = $derived(authRecord.teams.includes(event.team));
 
   // JS: splitting an empty string by comma returns length `1`
   const guestCount = $derived(event.guests === "" ? 0 : event.guests.split(",").length);
@@ -47,33 +49,33 @@
 
 <section class="flex justify-end items-end gap-2 flex-wrap">
   <button
-          aria-busy={submitting}
-          class="chip {chipClasses} preset-outlined-success-500 participation-button"
-          class:preset-filled-success-500={userParticipation.state === "in"}
-          disabled={submitting}
-          onclick={() => updateParticipationStatus("in")}
+    aria-busy={submitting}
+    class="chip {chipClasses} preset-outlined-success-500 participation-button"
+    class:preset-filled-success-500={userParticipation.state === "in"}
+    disabled={submitting || !canParticipate}
+    onclick={() => updateParticipationStatus("in")}
   >
     <span><Check size="14"/></span>
     <span>{(event.participations.in.length ?? 0) + guestCount}</span>
   </button>
 
   <button
-          aria-busy={submitting}
-          class="chip {chipClasses} preset-outlined-warning-500 participation-button"
-          class:preset-filled-warning-500={userParticipation.state === "maybe"}
-          disabled={submitting}
-          onclick={() => updateParticipationStatus("maybe")}
+    aria-busy={submitting}
+    class="chip {chipClasses} preset-outlined-warning-500 participation-button"
+    class:preset-filled-warning-500={userParticipation.state === "maybe"}
+    disabled={submitting || !canParticipate}
+    onclick={() => updateParticipationStatus("maybe")}
   >
     <span><CircleQuestionMark size="14"/></span>
     <span>{event.participations.maybe.length ?? 0}</span>
   </button>
 
   <button
-          aria-busy={submitting}
-          class="chip {chipClasses} preset-outlined-error-500 participation-button"
-          class:preset-filled-error-500={userParticipation.state === "out"}
-          disabled={submitting}
-          onclick={() => updateParticipationStatus("out")}
+    aria-busy={submitting}
+    class="chip {chipClasses} preset-outlined-error-500 participation-button"
+    class:preset-filled-error-500={userParticipation.state === "out"}
+    disabled={submitting || !canParticipate}
+    onclick={() => updateParticipationStatus("out")}
   >
     <span><X size="14"/></span>
     <span>{event.participations.out.length ?? 0}</span>
