6f15cc568b4bcb5424dbdf6d44cdffaea103c1b9
David Battefeld <post@davidbattefeld.de>
2026-02-06 18:24:14 +0100

    feat: notify all superusers via email when new users are created

--- dp/hooks_user.go
+++ dp/hooks_user.go
@@ -2,9 +2,12 @@ package dp
 
 import (
 	"fmt"
+	"net/mail"
 
 	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/tools/mailer"
+	"github.com/pocketbase/pocketbase/tools/template"
 	"github.com/spf13/cast"
 )
 
@@ -13,10 +16,65 @@ func NotifyAdminsUserCreation(e *core.RecordEvent, ps PushService) error {
 	if err != nil {
 		e.App.Logger().Warn(err.Error())
 	}
+	err = sendSuperUserNotification(e.Record, e.App)
+	if err != nil {
+		e.App.Logger().Warn(err.Error())
+	}
 
 	return e.Next()
 }
 
+func sendSuperUserNotification(record *core.Record, app core.App) error {
+	user := &User{}
+	user.SetProxyRecord(record)
+
+	superusers, err := app.FindAllRecords(core.CollectionNameSuperusers)
+	if err != nil {
+		app.Logger().Warn("Couldn't fetch superuser records for email notification", "error", err)
+		return err
+	}
+
+	client := app.NewMailClient()
+
+	html, err := template.NewRegistry().LoadFiles(
+		"templates/email/newUserCreated.gohtml",
+	).Render(map[string]any{
+		"username":  user.Username(),
+		"firstName": user.FirstName(),
+		"lastName":  user.LastName(),
+		"email":     user.Email(),
+		"signupKey": user.SignupKey(),
+		"teams":     user.Teams(),
+		"club":      user.Club(),
+		"url":       app.Settings().Meta.AppURL,
+	})
+	if err != nil {
+		app.Logger().Warn("error rendering superuser notification email template", "error", err)
+		return err
+	}
+
+	for _, superuser := range superusers {
+		message := &mailer.Message{
+			From: mail.Address{
+				Address: app.Settings().Meta.SenderAddress,
+				Name:    app.Settings().Meta.SenderName,
+			},
+			To:      []mail.Address{{Address: superuser.Email()}},
+			Subject: "New user created in Diamond Planner",
+			HTML:    html,
+			Text:    fmt.Sprintf("New user created in Diamond Planner: %s %s (%s). Email: %s, Signup Key: %s", user.FirstName(), user.LastName(), user.Username(), user.Email(), user.SignupKey()),
+		}
+
+		err = client.Send(message)
+		if err != nil {
+			app.Logger().Warn("Couldn't send email notification to superuser", "error", err, "email", superuser.Email())
+			return err
+		}
+	}
+	return nil
+}
+
+// notifyAdminsUserCreation sends a push notification to club admins when a new user is created
 func notifyAdminsUserCreation(record *core.Record, app core.App, ps PushService) error {
 	user := &User{}
 	user.SetProxyRecord(record)
