632525988b4a5988b116d4e993fdd1d9d27ab00b
David Battefeld <post@davidbattefeld.de>
2026-02-07 18:17:21 +0100

    fix: make FindRecordProxyByID() use pointers as parameter

--- dp/record_proxy.go
+++ dp/record_proxy.go
@@ -15,11 +15,12 @@ type AppRecordProxy interface {
 
 // FindRecordProxyByID retrieves a record proxy by its ID from the database. Simple Wrapper for `app.FindRecordById()` to
 // prevent having to load the result manually into a proxy object.
+// Caution: `proxy` needs to be a pointer to a struct that implements AppRecordProxy, not a value.
 func FindRecordProxyByID(app core.App, proxy AppRecordProxy, id string) error {
 	err := app.RecordQuery(proxy.CollectionName()).
 		AndWhere(dbx.HashExp{"id": id}).
 		Limit(1).
-		One(&proxy)
+		One(proxy)
 	if err != nil {
 		return err
 	}
