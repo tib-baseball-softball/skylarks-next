ad8ecfa115537f50e97ccb3e71d865c76e969f80
David Battefeld <post@davidbattefeld.de>
2026-02-07 11:09:13 +0100

    refactor: introduce constants for timezones

--- dp/date_time_utility.go
+++ dp/date_time_utility.go
@@ -7,6 +7,11 @@ import (
 	"github.com/pocketbase/pocketbase/tools/types"
 )
 
+const (
+	TimeZoneEnvKey  = "TIME_ZONE"
+	DefaultTimezone = "Europe/Berlin"
+)
+
 func GetYearStartAndEnd(year int) (start, end types.DateTime, err error) {
 	startTime := time.Date(year, time.January, 1, 0, 0, 0, 0, time.UTC)
 	endTime := time.Date(year, time.December, 31, 23, 59, 59, 0, time.UTC)
@@ -24,9 +29,9 @@ func GetYearStartAndEnd(year int) (start, end types.DateTime, err error) {
 }
 
 func LoadAppTimeZone() (*time.Location, error) {
-	timezone := os.Getenv("TIME_ZONE")
+	timezone := os.Getenv(TimeZoneEnvKey)
 	if timezone == "" {
-		timezone = "Europe/Berlin"
+		timezone = DefaultTimezone
 	}
 
 	location, err := time.LoadLocation(timezone)
