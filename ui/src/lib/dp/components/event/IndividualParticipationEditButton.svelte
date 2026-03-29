1fb9932f8b5cecaad27d764850fb6bfc7faaa419
David Battefeld <post@davidbattefeld.de>
2026-03-30 00:13:15 +0200

    fix: close popover on edit button click

--- ui/src/lib/dp/components/event/IndividualParticipationEditButton.svelte
+++ ui/src/lib/dp/components/event/IndividualParticipationEditButton.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-  import {MessageCircleMore} from "lucide-svelte";
+  import { MessageCircleMore } from "lucide-svelte";
   import ParticipationForm from "$lib/dp/components/forms/ParticipationForm.svelte";
   import Dialog from "$lib/dp/components/modal/Dialog.svelte";
-  import {authSettings} from "$lib/dp/client.svelte.js";
-  import type {CustomAuthModel, ExpandedParticipation} from "$lib/dp/types/ExpandedResponse.ts";
+  import { authSettings } from "$lib/dp/client.svelte.js";
+  import type {
+    CustomAuthModel,
+    ExpandedParticipation,
+  } from "$lib/dp/types/ExpandedResponse.ts";
+  import type { HTMLButtonAttributes } from "svelte/elements";
 
   interface Props {
     participation: ExpandedParticipation;
@@ -11,31 +15,45 @@
     classes?: string;
   }
 
-  const {participation, isAdmin, classes = ""}: Props = $props();
+  const { participation, isAdmin, classes = "" }: Props = $props();
 
   const authRecord = $derived(authSettings.record as CustomAuthModel);
 
   const canEdit = $derived(participation.user === authRecord.id || isAdmin);
 
-  const id = Math.random().toString(36).substring(3, 9);
+  const id = Math.random().toString(36).substring(3, 16);
 
-  const createdDate =
-    $derived(participation.created !== "" ? new Date(participation.created).toLocaleString() : "---");
-  const updatedDate =
-    $derived(participation.updated !== "" ? new Date(participation.updated).toLocaleString() : "---");
+  const createdDate = $derived(
+    participation.created !== ""
+      ? new Date(participation.created).toLocaleString()
+      : "---",
+  );
+  const updatedDate = $derived(
+    participation.updated !== ""
+      ? new Date(participation.updated).toLocaleString()
+      : "---",
+  );
+
+  const triggerProps: {
+    popovertarget: HTMLButtonAttributes["popovertarget"];
+    popovertargetaction: HTMLButtonAttributes["popovertargetaction"];
+  } = {
+    popovertarget: id,
+    popovertargetaction: "hide",
+  };
 </script>
 
 <button class="{classes} trigger-button" popovertarget={id}>
   {#if participation.comment}
-          <span class="badge-icon preset-filled-surface-400-600">
-            <MessageCircleMore size="13"/>
-          </span>
+    <span class="badge-icon preset-filled-surface-400-600">
+      <MessageCircleMore size="13" />
+    </span>
   {/if}
-  {participation?.expand?.user?.display_name || participation?.expand?.user?.first_name}
+  {participation?.expand?.user?.display_name ||
+    participation?.expand?.user?.first_name}
 </button>
 
 <div class="popover-content card shadow-2xl" popover="auto" {id}>
-
   <div class="item-container">
     <p class="label-text">Created:</p>
     <p>{createdDate}</p>
@@ -52,8 +70,10 @@
   </div>
 
   {#if canEdit}
-    <Dialog triggerClasses="btn btn-sm preset-tonal-primary border border-primary-500 edit-btn">
-
+    <Dialog
+      triggerClasses="btn btn-sm preset-tonal-primary border border-primary-500 edit-btn"
+      {triggerProps}
+    >
       {#snippet triggerContent()}
         Edit
       {/snippet}
@@ -61,13 +81,14 @@
       {#snippet title()}
         <header>
           <h2 class="h3">
-            Edit participation data
-            for {participation?.expand?.user?.first_name} {participation?.expand?.user?.last_name}
+            Edit participation data for {participation?.expand?.user
+              ?.first_name}
+            {participation?.expand?.user?.last_name}
           </h2>
         </header>
       {/snippet}
 
-      <ParticipationForm {participation}/>
+      <ParticipationForm {participation} />
     </Dialog>
   {/if}
 </div>
@@ -83,7 +104,10 @@
     top: anchor(right, var(--spacing));
     border: 1px solid;
     color: light-dark(black, white);
-    background-color: light-dark(var(--color-surface-50), var(--color-surface-800));
+    background-color: light-dark(
+      var(--color-surface-50),
+      var(--color-surface-800)
+    );
     padding: calc(var(--spacing) * 4);
     font-size: var(--text-sm);
     line-height: var(--tw-leading, var(--text-sm--line-height));
@@ -102,7 +126,11 @@
     transform: translateY(-50px);
     opacity: 0;
 
-    transition: transform, opacity, display allow-discrete, overlay allow-discrete;
+    transition:
+      transform,
+      opacity,
+      display allow-discrete,
+      overlay allow-discrete;
     transition-duration: 0.2s;
 
     @media (prefers-reduced-motion: reduce) {
