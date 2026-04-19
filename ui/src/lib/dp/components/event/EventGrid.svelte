a03da0209e1698c3ae44ad69fe228eaedc6ea43e
David Battefeld <post@davidbattefeld.de>
2026-04-19 17:46:38 +0200

    refactor: create reusable event grid component

---
+++ ui/src/lib/dp/components/event/EventGrid.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import EventTeaser from "$lib/dp/components/event/EventTeaser.svelte";
+  import type {ExpandedEvent} from "$lib/dp/types/ExpandedResponse.ts";
+
+  interface Props {
+    events: ExpandedEvent[];
+  }
+
+  const {events}: Props = $props();
+</script>
+
+<div class="events-grid">
+  {#each events as event (event.id)}
+    <div>
+      <EventTeaser {event} link={true}/>
+    </div>
+  {:else}
+    <p>No events available with the current filters.</p>
+  {/each}
+</div>
+
+<style>
+  .events-grid {
+    margin-block: calc(var(--spacing) * 4);
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: calc(var(--spacing) * 4);
+
+    @media (min-width: 48rem) {
+      grid-template-columns: 1fr 1fr;
+    }
+
+    @media (min-width: 80rem) {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+  }
+</style>
