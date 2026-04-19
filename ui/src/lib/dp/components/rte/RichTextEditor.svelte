91ff0eb68753f3adae3c003da7b088d1ab921cd1
David Battefeld <post@davidbattefeld.de>
2026-04-19 17:07:46 +0200

    dev: add RTE shell

---
+++ ui/src/lib/dp/components/rte/RichTextEditor.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import {onMount} from "svelte";
+
+  interface Props {
+    content: string;
+  }
+
+  let {content = $bindable()}: Props = $props();
+
+  let editorElement: HTMLElement;
+
+  onMount(() => {
+
+  });
+
+</script>
+
+<div bind:this={editorElement}></div>
