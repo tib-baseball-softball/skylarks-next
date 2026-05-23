60436f8dec5a17240c38c3bf94e7c4e0a0a0aace
David Battefeld <post@davidbattefeld.de>
2026-05-23 10:47:08 +0200

    feat: add DB migrations for markdown fields

---
+++ migrations/1779525366_updated_announcements.go
@@ -0,0 +1,46 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3866499052")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(9, []byte(`{
+			"autogeneratePattern": "",
+			"help": "",
+			"hidden": false,
+			"id": "text4235653420",
+			"max": 8000,
+			"min": 0,
+			"name": "TEMP_BODYTEXT_DATA",
+			"pattern": "",
+			"presentable": false,
+			"primaryKey": false,
+			"required": false,
+			"system": false,
+			"type": "text"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3866499052")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("text4235653420")
+
+		return app.Save(collection)
+	})
+}
