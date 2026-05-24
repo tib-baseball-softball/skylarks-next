5bb745f91e9794311c19dd90392de32bf90e4afc
David Battefeld <post@davidbattefeld.de>
2026-05-24 09:44:47 +0200

    fix: fix dark mode for RTE

--- ui/src/lib/dp/components/rte/RichTextEditor.svelte
+++ ui/src/lib/dp/components/rte/RichTextEditor.svelte
@@ -2,6 +2,7 @@
   import {Carta, MarkdownEditor} from 'carta-md';
   import 'carta-md/default.css';
   import DOMPurify from 'dompurify';
+  import {onMount} from "svelte";
 
   interface Props {
     value: string;
@@ -12,6 +13,19 @@
 
   const carta = new Carta({
     sanitizer: DOMPurify.sanitize,
+  });
+
+  onMount(() => {
+    // this is a hack, for some reason no classes can be added via the component
+    const cartaRenderers = document.querySelectorAll('.carta-renderer.markdown-body');
+    if (cartaRenderers.length === 0) {
+      console.warn('Carta renderer elements not found');
+      return;
+    }
+
+    cartaRenderers.forEach((renderer) => {
+      renderer.classList.add("prose");
+    });
   });
 </script>
 
